9d73ada5624f26604adba4b84eb735dce172c26b
Daniel Salin <daniel.salin@chasacademy.com>
2020-01-23 10:02:50 +0100

    Add line chart for Details component
    Using static sample data in state to render chart data

--- src/components/Monitor.js
+++ src/components/Monitor.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Chart from 'chart.js';
 
-export default function Monitor() {
-    return (
-        <div className="bg-secondary container">
-            <h2>Monitor</h2>
-        </div>
-    )
+export default class Monitor extends Component {
+    chartRef = React.createRef();
+    constructor(props) {
+        super(props);
+        this.state = {
+            "months": ["June", "July", "August", "September", "October", "November"],
+            "values": [-5, -43, -34, -49, -25, -19]
+        }
+    }
+
+    componentDidMount() {
+        const detailsChartRef = this.chartRef.current.getContext("2d");
+            Chart.defaults.global.defaultFontColor = '#7C7C7C';
+          Chart.defaults.global.defaultFontFamily = 'Roboto';
+          Chart.defaults.global.defaultFontSize = 14;
+
+        new Chart(detailsChartRef, {
+            type: 'line',
+            data: {
+                labels: this.state.months,
+                datasets: [{
+                    data: this.state.values,
+                    borderColor: [
+                        'rgba(250, 191, 44, 1)'
+                    ],
+                    borderWidth: 3,
+                    pointRadius: 5,
+                    pointBorderWidth: 3,
+                    pointBackgroundColor: 'rgba(250, 191, 44, 1)',
+                    fill: false
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                          fontColor: 'pink',
+                        },
+                        ticks: {
+                          beginAtZero: false,
+                          suggestedMax: 30,
+                          suggestedMin: -60,
+                        }
+                    }],
+
+                },
+                legend: {
+                    display: false
+                },
+                fill: false
+                // responsive: false,
+            }
+        });
+    }
+    render() {
+        return (
+            <div className="chart__nps">
+                <canvas 
+                id="chart__nps"
+                ref={this.chartRef}
+                />
+            </div>
+        )
+    }
 }
