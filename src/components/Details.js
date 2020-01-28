7f8b5c33f8d8dccd88458cebcbe4759b82c2a720
Daniel Salin <daniel.salin@chasacademy.com>
2020-01-28 15:12:41 +0100

    Implement Progressbar component into Details component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Progressbar from './Progressbar';
 
-export default function Details() {
-    return (
-        <div className="bg-danger container">
-            <h2>Details</h2>            
-        </div>
-    )
+export default class Details extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "clients": [
+                {
+                    "name": "Arvid Nordqvist",
+                    "progress": "3"
+                },
+                {
+                    "name": "Bauhaus1",
+                    "progress": "0"
+                },
+                {
+                    "name": "Björn Borg",
+                    "progress": "3"
+                },
+                {
+                    "name": "Dr Denim",
+                    "progress": "5"
+                },
+                {
+                    "name": "Elon",
+                    "progress": "7"
+                },
+                {
+                    "name": "Engelsson",
+                    "progress": "9"
+                },
+                {
+                    "name": "Hydroscand",
+                    "progress": "10"
+                },
+                {
+                    "name": "Lantmännen",
+                    "progress": "8"
+                },
+                {
+                    "name": "Sc Motors",
+                    "progress": "6"
+                },
+                {
+                    "name": "Stiga Sports",
+                    "progress": "5"
+                }
+            ]
+        }
+    }
+
+
+    render() {
+        const { clients } = this.state;
+
+        return (
+            <div>
+                <ol>
+                    {clients.map((client, index) => {
+                        return (
+                            <Progressbar clientName={client.name} clientScore={client.progress} />
+                        )
+                    })}
+                </ol>
+            </div>
+        )
+    }
 }
