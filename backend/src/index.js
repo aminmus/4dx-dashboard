11beb0717b63d343c33ce1742c9038ccfea27768
Amin Mushehid <amin.mushehid@vaimo.com>
2020-01-29 13:24:01 +0100

    Add basic express server

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 4000;
+
+app.use((_req, res) => {
+    res.writeHead(200);
+    res.end('Hello, World!\n');
+  });
+
+  app.listen(port, () => console.log(`Listening on port ${port}!`));
+  
