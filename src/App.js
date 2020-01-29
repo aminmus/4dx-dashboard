48f78701b6fc938123514dccecd3589ce372b62f
Daniel Salin <daniel.salin@chasacademy.com>
2020-01-29 12:56:16 +0100

    main-view Move boostrap import to prioritize custom css, fixes to App component, add css styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './layout/Home';
 import Admin from './layout/Admin';
 import logo from './logo.png';
@@ -8,22 +7,22 @@ import logo from './logo.png';
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-          <a class="navbar-brand" href="#">
-            <img src={logo} height="105" alt="" />
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-light py-0">
+          <a className="navbar-brand" href="#">
+            <img className="logo" src={logo} alt="" />
           </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
 
-              <li class="nav-item">
-                <a class="nav-link" href="#"><Link className="text-light" style={{ textDecoration: 'none' }} to="/">Home</Link></a>
+              <li className="nav-item">
+                <a className="nav-link" href="#"><Link className="text-light" style={{ textDecoration: 'none' }} to="/">Home</Link></a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"><Link className="text-light" style={{ textDecoration: 'none' }} to="/admin">Admin</Link></a>
+              <li className="nav-item">
+                <a className="nav-link" href="#"><Link className="text-light" style={{ textDecoration: 'none' }} to="/admin">Admin</Link></a>
               </li>
             </ul>
           </div>
@@ -37,7 +36,7 @@ export default function App() {
             <Home />
           </Route>
         </Switch>
-      </div>
+      </header>
     </Router>
   );
 }
