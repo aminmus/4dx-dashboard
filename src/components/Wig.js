fe7b8e483c21f271f6973bcbd34ad4d4cdffb989
Daniel Salin <daniel.salin@chasacademy.com>
2020-01-29 12:59:37 +0100

    main-view Update Wig component with design and template output

--- src/components/Wig.js
+++ src/components/Wig.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Wig() {
-    return (
-        <div className="bg-primary container">
-            <h2>Wig</h2>
+export default class Wig extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "nps":
+            {
+                "description": "From -42 NPS to 10 by the end of October 2019",
+                "current": "-18",
+                "goal": "8",
+                "defineClients": "5",
+                "defineText": "Define the Success factors for top 10 clients",
+                "implementText": "Implement Client Success Program for top 10 clients"
+            },
+        }
+    }
 
-        </div>
-    )
+    render() {
+        const { current, description } = this.state.nps;
+        return (
+            <div className="wig bg-container text-center">
+                <h2>WIG</h2>
+                <h3 className="wig__statement">{description}</h3>
+                <div className="wig__chart">
+                    <div className="chart__data" >
+                        <span className="wig__label" >NPS</span>
+                        <span className="wig__result">{current}</span>
+                    </div>
+                    <div style={this.wigChartAfter}></div>
+                </div>
+            </div>
+        )
+    }
 }
