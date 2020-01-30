415bd08533e938a0668be8814931cb97b004f5c1
Daniel Salin <daniel.salin@chasacademy.com>
2020-01-30 09:57:16 +0100

    Implement Context Consumer and remove local state from Details component
    -Convert Details from Class to Functional component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,69 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import StateContext from '../context/state-context';
 import Progressbar from './Progressbar';
 
-export default class Details extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            "clients": [
-                {
-                    "name": "Arvid Nordqvist",
-                    "progress": "3"
-                },
-                {
-                    "name": "Bauhaus1",
-                    "progress": "0"
-                },
-                {
-                    "name": "Björn Borg",
-                    "progress": "3"
-                },
-                {
-                    "name": "Dr Denim",
-                    "progress": "5"
-                },
-                {
-                    "name": "Elon",
-                    "progress": "7"
-                },
-                {
-                    "name": "Engelsson",
-                    "progress": "9"
-                },
-                {
-                    "name": "Hydroscand",
-                    "progress": "10"
-                },
-                {
-                    "name": "Lantmännen",
-                    "progress": "8"
-                },
-                {
-                    "name": "Sc Motors",
-                    "progress": "6"
-                },
-                {
-                    "name": "Stiga Sports",
-                    "progress": "5"
-                }
-            ]
-        }
-    }
-
-
-    render() {
-        const { clients } = this.state;
-
+export default function Details() {
         return (
-            <div>
-                <ol>
-                    {clients.map((client, index) => {
-                        return (
-                            <Progressbar clientName={client.name} clientScore={client.progress} />
-                        )
-                    })}
-                </ol>
-            </div>
+            <StateContext.Consumer>
+            {context => (
+                <div>
+                    <ol>
+                        {context.clients.map((client) => {
+                            return (
+                                <Progressbar 
+                                clientName={client.name} 
+                                clientScore={client.progress} />
+                            )
+                        })}
+                    </ol>
+                </div>
+            )}
+            </StateContext.Consumer>
         )
-    }
 }
