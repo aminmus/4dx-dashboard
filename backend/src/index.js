b84f06871b6944710ef22b0dcaf4c22a93249809
Daniel Salin <daniel.salin@chasacademy.com>
2020-02-04 15:42:10 +0100

    Add new client draft route and cors middleware to BE

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 4000;
 
-app.use('/api', require('./routes/nps'));
+app.use(cors());
 
-app.use('/', (_req, res) => {
-  res.writeHead(200);
-  res.end('Hello, World!\n');
+app.use("/api", require("./routes/nps"));
+app.use("/api", require("./routes/clients"));
+
+app.use("/", (_req, res) => {
+	res.writeHead(200);
+	res.end("Hello, World!\n");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
--- backend/src/routes/clients.js
+++ backend/src/routes/clients.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 
 const data = require("../../data.json");
 
-router.get("/nps", (_req, res) => {
+router.get("/clients", (_req, res) => {
 	res.setHeader("X-Total-Count", "30");
 	res.setHeader("Access-Control-Expose-Headers", "X-Total-Count");
-	res.send(data.nps);
+	res.send(data.clients);
 });
 
 module.exports = router;
