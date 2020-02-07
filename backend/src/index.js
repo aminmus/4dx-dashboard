088355f887ecac721555804f10309a03ddf703cc
Amin Mushehid <amin.mushehid@vaimo.com>
2020-02-07 14:37:22 +0100

    Rewrite trying of database connection and order of console logs

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,32 +2,33 @@ const express = require('express');
 
 require('dotenv').config();
 
+const tryDbConnection = require('./utils/tryDbConnection');
+
 const db = require('../models');
 
-// Authenticate Connection Database
-try {
-  db.sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect o the database:', error);
-}
+const startServer = async () => {
+  await tryDbConnection();
 
-// Sync Database
-try {
-  db.sequelize.sync();
-  console.error('Sync complete');
-} catch (error) {
-  console.error('Unable to sync:', error);
-}
+  // Sync Database
+  try {
+    console.log('Trying to sync models to database...');
+    await db.sequelize.sync();
+    console.log('Database sync completed');
+  } catch (error) {
+    console.error('Unable to sync database:', error);
+  }
 
-const app = express();
+  const app = express();
 
-app.use('/api', require('./routes/client'));
-app.use('/api', require('./routes/user'));
+  app.use('/api', require('./routes/client'));
+  app.use('/api', require('./routes/user'));
 
-app.use('/', (_req, res) => {
-  res.writeHead(200);
-  res.end('Hello, World!\n');
-});
+  app.use('/', (_req, res) => {
+    res.writeHead(200);
+    res.end('Hello, World!\n');
+  });
 
-app.listen(process.env.SERVER_PORT, () => console.log(`Listening on port ${process.env.SERVER_PORT}!`));
+  app.listen(process.env.SERVER_PORT, () => console.log(`Listening on port ${process.env.SERVER_PORT}!`));
+};
+
+startServer();
---
+++ backend/src/utils/tryDbConnection.js
@@ -0,0 +1,22 @@
+const db = require('../../models/index');
+
+const tryDbConnection = async () => {
+  let retries = 5;
+  while (retries) {
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      await db.sequelize.authenticate();
+      console.log('Established connecion to database');
+      break;
+    } catch (err) {
+      retries -= 1;
+      console.log(err);
+      console.log(`retries left: ${retries}`);
+      // wait 5 seconds
+      // eslint-disable-next-line no-await-in-loop
+      await new Promise((res) => setTimeout(res, 5000));
+    }
+  }
+};
+
+module.exports = tryDbConnection;
