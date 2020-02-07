2bd5afa2d63d3e530e599e976974be50385a592b
Amin Mushehid <amin.mushehid@vaimo.com>
2020-02-07 13:40:22 +0100

    Fix code style issues of index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,33 @@
-const express = require("express");
+const express = require('express');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const db = require("../models");
+const db = require('../models');
 
 // Authenticate Connection Database
 try {
-	db.sequelize.authenticate();
-	console.log("Connection has been established successfully.");
+  db.sequelize.authenticate();
+  console.log('Connection has been established successfully.');
 } catch (error) {
-	console.error("Unable to connect to the database:", error);
+  console.error('Unable to connect o the database:', error);
 }
 
 // Sync Database
 try {
-	db.sequelize.sync();
-	console.error("Sync complete");
+  db.sequelize.sync();
+  console.error('Sync complete');
 } catch (error) {
-	console.error("Unable to sync:", error);
+  console.error('Unable to sync:', error);
 }
 
 const app = express();
 
-app.use("/api", require("./routes/client"));
-app.use("/api", require("./routes/user"));
+app.use('/api', require('./routes/client'));
+app.use('/api', require('./routes/user'));
 
-app.use("/", (_req, res) => {
-	res.writeHead(200);
-	res.end("Hello, World!\n");
+app.use('/', (_req, res) => {
+  res.writeHead(200);
+  res.end('Hello, World!\n');
 });
 
-app.listen(process.env.SERVER_PORT, () =>
-	console.log(`Listening on port ${process.env.SERVER_PORT}!`)
-);
+app.listen(process.env.SERVER_PORT, () => console.log(`Listening on port ${process.env.SERVER_PORT}!`));
