6c777a8a4117344a38fed9421b571c5f3de9ad6a
Daniel Salin <daniel.salin@chasacademy.com>
2020-02-07 11:27:07 +0100

    Add sequalize config and implement database connection and sync

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,34 @@
-const express = require('express');
+const express = require("express");
 
-require('dotenv').config();
+require("dotenv").config();
+
+const db = require("../models");
+
+// Authenticate Connection Database
+try {
+	db.sequelize.authenticate();
+	console.log("Connection has been established successfully.");
+} catch (error) {
+	console.error("Unable to connect to the database:", error);
+}
+
+// Sync Database
+try {
+	db.sequelize.sync();
+	console.error("Sync complete");
+} catch (error) {
+	console.error("Unable to sync:", error);
+}
 
 const app = express();
 
-app.use('/api', require('./routes/nps'));
+app.use("/api", require("./routes/nps"));
 
-app.use('/', (_req, res) => {
-  res.writeHead(200);
-  res.end('Hello, World!\n');
+app.use("/", (_req, res) => {
+	res.writeHead(200);
+	res.end("Hello, World!\n");
 });
 
-app.listen(process.env.SERVER_PORT, () => console.log(`Listening on port ${process.env.SERVER_PORT}!`));
+app.listen(process.env.SERVER_PORT, () =>
+	console.log(`Listening on port ${process.env.SERVER_PORT}!`)
+);
