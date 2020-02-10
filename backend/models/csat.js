3eefa004d086f8e937fed57c806830ab20a96dfe
Amin Mushehid <amin.mushehid@vaimo.com>
2020-02-10 10:36:26 +0100

    Fix all easily fixable eslint errors

--- backend/models/csat.js
+++ backend/models/csat.js
@@ -1,15 +1,15 @@
-"use strict";
+
 module.exports = (sequelize, DataTypes) => {
-	const Csat = sequelize.define(
-		"Csat",
-		{
-			score: DataTypes.INTEGER,
-			date: DataTypes.DATEONLY
-		},
-		{}
-	);
-	Csat.associate = function(models) {
-		Csat.belongsTo(models.Client);
-	};
-	return Csat;
+  const Csat = sequelize.define(
+    'Csat',
+    {
+      score: DataTypes.INTEGER,
+      date: DataTypes.DATEONLY,
+    },
+    {},
+  );
+  Csat.associate = function (models) {
+    Csat.belongsTo(models.Client);
+  };
+  return Csat;
 };
--- backend/src/routes/api/nps.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const npsData = require('../../data.json');
-
-router.get('/nps', (_req, res) => {
-  res.send(npsData);
-});
-
-module.exports = router;
