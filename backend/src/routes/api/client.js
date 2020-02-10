80f818e4ad9694c9b66004cb88aa92a9baef23c0
Amin Mushehid <amin.mushehid@vaimo.com>
2020-02-10 10:29:45 +0100

    Correct directories in imports, and add a try catch block
    Modules were not being found because of previous directory changes.
    Also, an exception was being thrown but not catched regarding client route and associated/unassociated measures.

--- backend/src/routes/api/client.js
+++ backend/src/routes/api/client.js
@@ -1,18 +1,25 @@
-const router = require("express").Router();
-const db = require("../../../models/client.js");
+const router = require('express').Router();
+const db = require('../../../models/index');
 
-const Client = db.sequelize.import("../../models/client");
-const Measure = db.sequelize.import("../../models/measure");
+const Client = db.sequelize.import('../../../models/client');
+const Measure = db.sequelize.import('../../../models/measure');
 
-router.get("/clients", async (_req, res) => {
-	const client = await Client.findAll({
-		include: [
-			{
-				model: Measure
-			}
-		]
-	});
-	res.send(client);
+router.get('/clients', async (_req, res) => {
+  try {
+    const client = await Client.findAll({
+      include: [
+        {
+          model: Measure,
+        },
+      ],
+    });
+    return client;
+  } catch (error) {
+    return res.send({
+      'error message': error.message,
+      error,
+    });
+  }
 });
 
 module.exports = router;
--- backend/src/routes/api/user.js
+++ backend/src/routes/api/user.js
@@ -1,10 +1,11 @@
-const router = require("express").Router();
-const db = require("../../../models/index");
-const User = db.sequelize.import("../../models/user");
+const router = require('express').Router();
+const db = require('../../../models/index');
 
-router.get("/users", async (_req, res) => {
-	const users = await User.findAll();
-	res.send(users);
+const User = db.sequelize.import('../../../models/user');
+
+router.get('/users', async (_req, res) => {
+  const users = await User.findAll();
+  res.send(users);
 });
 
 module.exports = router;
