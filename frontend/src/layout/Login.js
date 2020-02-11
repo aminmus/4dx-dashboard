acbd0829544726e995191e916e52d105e5c4b705
Daniel Salin <daniel.salin@chasacademy.com>
2020-02-11 13:07:35 +0100

    Update Login component
    -Replace jsonServerProvider with simpleRestProvider for REST-api
    -Add material theme to Admin
    - Use updated Resource components for clients, csat, measures and users

--- frontend/src/layout/Login.js
+++ frontend/src/layout/Login.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
-import { ClientList, ClientCreate, ClientEdit } from '../components/dashboard/client';
-import { NpsEdit, NpsList, NpsCreate } from '../components/dashboard/nps';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import { ClientList, ClientEdit, ClientShow, ClientCreate } from '../components/dashboard/client';
 import authProvider from '../authProvider';
 import Dashboard from './Dashboard';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { CsatEdit, CsatCreate } from '../components/dashboard/csat';
+import { MeasureEdit, MeasureCreate } from '../components/dashboard/measure';
+import simpleRestProvider from 'ra-data-simple-rest';
 
 export default function Login() {
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark'
+    }
+  });
+
   return (
     <Admin
       authProvider={authProvider}
-      dataProvider={jsonServerProvider('http://localhost:4000/api')}
+      dataProvider={simpleRestProvider('http://localhost:4000/api')}
       dashboard={Dashboard}
+      theme={theme}
     >
-      <Resource name="clients" list={ClientList} edit={ClientEdit} create={ClientCreate} />
-      <Resource name="nps" list={NpsList} edit={NpsEdit} create={NpsCreate} />
+      <Resource
+        name="clients"
+        create={ClientCreate}
+        list={ClientList}
+        edit={ClientEdit}
+        show={ClientShow}
+      />
+      <Resource name="csat" edit={CsatEdit} create={CsatCreate} />
+      <Resource name="measures" edit={MeasureEdit} create={MeasureCreate} />
+      <Resource name="users" list={ListGuesser} />
     </Admin>
   );
 }
