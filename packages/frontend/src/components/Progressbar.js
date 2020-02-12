64341ceeb6ab3b205a3788b6f3c7fc57653e4905
Daniel Salin <daniel.salin@chasacademy.com>
2020-02-12 07:26:59 +0100

    Add color range to Progressbar

--- packages/frontend/src/components/Progressbar.js
+++ packages/frontend/src/components/Progressbar.js
@@ -13,7 +13,20 @@ export default function ProgressBar(props) {
     const x = parseInt(progress) / parseInt(total);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = '#E5000A';
+    switch (true) {
+      case x >= 0.2 && x < 0.4:
+        ctx.fillStyle = '#DB6200';
+        break;
+      case x >= 0.4 && x < 0.6:
+        ctx.fillStyle = '#D2C500';
+        break;
+      case x >= 0.6 && x < 0.8:
+        ctx.fillStyle = '#70C800';
+        break;
+      default:
+        ctx.fillStyle = '#0CBF00';
+    }
     ctx.fillRect(0, 0, canvas.width * x, canvas.height);
   });
 
