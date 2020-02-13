259b806626fb5375cf97b1c3093b46e53d409421
Amin Mushehid <amin.mushehid@vaimo.com>
2020-02-13 13:39:26 +0100

    Create signup middleware

---
+++ packages/backend/src/auth/auth.js
@@ -0,0 +1,16 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const UserModel = require('../../models/user');
+
+passport.use('signup', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+}, async (email, password, done) => {
+  try {
+    const user = await UserModel.create({ email, password });
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
+}));
