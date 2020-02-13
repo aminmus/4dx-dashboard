cdf43515faf551816532de44bb1f571a8a46d445
Amin Mushehid <amin.mushehid@vaimo.com>
2020-02-13 12:33:41 +0100

    Add password hashing hook and comparison method on User Model

--- packages/backend/models/user.js
+++ packages/backend/models/user.js
@@ -1,18 +1,28 @@
-/* eslint-disable func-names */
+/* eslint-disable no-param-reassign */
+const Sequelize = require('sequelize');
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'User',
-    {
-      email: {
-        type: DataTypes.STRING,
-        unique: true,
-      },
-      password: DataTypes.STRING,
+  class User extends Sequelize.Model {
+    isValidPassword(password) {
+      return bcrypt.compare(password, this.password);
+    }
+  }
+  User.init({
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
     },
-    {},
-  );
+    password: DataTypes.STRING,
+  }, { sequelize });
+
   User.associate = function (_models) {
     // associations can be defined here
   };
+
+  User.beforeCreate(async (user) => {
+    user.password = await bcrypt.hash(user.password, 10);
+  });
+
   return User;
 };
