c311e5a40dfc46615ed16e1bc24a644bad6b466d
Amin Mushehid <amin.mushehid@vaimo.com>
2020-02-13 16:09:23 +0100

    Add login route

--- packages/backend/src/routes/api/auth.js
+++ packages/backend/src/routes/api/auth.js
@@ -1,10 +1,29 @@
 const router = require('express').Router();
 const passport = require('passport');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-router.post('signup', passport.authenticate('signup', { session: false }), async (req, res) => {
+router.post('/signup', passport.authenticate('signup', { session: false }), async (req, res) => {
   res.json({
     message: 'Signup successful',
     user: req.user,
   });
+});
+
+
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('login', async (err, user) => {
+    try {
+      if (err || !user) return next(err);
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error)
+
+        //Sign and populate token
+        const token = jwt.sign({ user: { id: user.id, email: user.email } }, process.env.SECRET_JWT_KEY, { expiresIn: '24h' });
+        return res.json({ token });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
 });
