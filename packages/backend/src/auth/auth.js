8df51ea079f0055389f0e0dca9e79db16ebbfd83
Amin Mushehid <amin.mushehid@vaimo.com>
2020-02-13 13:55:33 +0100

    Create login middleware

--- packages/backend/src/auth/auth.js
+++ packages/backend/src/auth/auth.js
@@ -14,3 +14,20 @@ passport.use('signup', new LocalStrategy({
     return done(error);
   }
 }));
+
+passport.use('login', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+}, async (email, password, done) => {
+  try {
+    const user = await UserModel.findOne({ where: { email } });
+    if (!user) return done(null, false, { message: 'User not found' });
+
+    const isValidPassword = await user.isValidPassword(password);
+    if (!isValidPassword) return done(null, false, { message: 'Invalid password' });
+
+    return done(null, user, { message: 'Logged in Successfully' });
+  } catch (error) {
+    return done(error);
+  }
+}));
