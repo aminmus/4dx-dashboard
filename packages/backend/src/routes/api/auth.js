9d8500ea49e4eae155c9df302b5a9ebe2880ae33
Amin Mushehid <amin.mushehid@vaimo.com>
2020-02-14 14:43:26 +0100

    Add auth routes under /api/auth

--- packages/backend/src/routes/api/auth.js
+++ packages/backend/src/routes/api/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
-router.post('/signup', passport.authenticate('signup', { session: false }), async (req, res) => {
+router.post('/auth/signup', passport.authenticate('signup', { session: false }), async (req, res) => {
   res.json({
     message: 'Signup successful',
     user: req.user,
@@ -10,16 +10,16 @@ router.post('/signup', passport.authenticate('signup', { session: false }), asyn
 });
 
 
-router.post('/login', async (req, res, next) => {
-  passport.authenticate('login', async (err, user) => {
+router.post('/auth/login', async (req, res, next) => {
+  passport.authenticate('login', (err, user) => {
     try {
       if (err || !user) return next(err);
 
-      req.login(user, { session: false }, async (error) => {
-        if (error) return next(error)
+      return req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
 
-        //Sign and populate token
-        const token = jwt.sign({ user: { id: user.id, email: user.email } }, process.env.SECRET_JWT_KEY, { expiresIn: '24h' });
+        // Sign and populate token
+        const token = jwt.sign({ user: { id: user.id, email: user.email } }, process.env.JWT_SECRET, { expiresIn: '24h' });
         return res.json({ token });
       });
     } catch (error) {
@@ -27,3 +27,5 @@ router.post('/login', async (req, res, next) => {
     }
   })(req, res, next);
 });
+
+module.exports = router;
