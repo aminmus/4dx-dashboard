005ea337c064052c5b84c3b2c028158a4c5ef091
Amin Mushehid <amin.mushehid@vaimo.com>
2020-02-16 20:42:32 +0100

    Move authentication route callbacks into controller file

--- packages/backend/src/routes/auth.js
+++ packages/backend/src/routes/auth.js
@@ -1,32 +1,8 @@
 const router = require('express').Router();
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
+const { signup, login } = require('../controllers/authentication');
 
-router.post('/signup', async (req, res, next) => {
-  passport.authenticate('signup', { session: false }, (err, user, info) => {
-    if (err) return next(err);
-    if (!user) return res.send(info);
+router.post('/signup', signup);
 
-    return res.json({
-      message: 'Signup successful',
-      user,
-    });
-  })(req, res, next);
-});
-
-router.post('/login', async (req, res, next) => {
-  passport.authenticate('login', (err, user, info) => {
-    try {
-      if (err) return next(err);
-      if (!user) return res.status(400).json(info);
-
-      // Sign and populate token
-      const token = jwt.sign({ user: { id: user.id, email: user.email } }, process.env.JWT_SECRET, { expiresIn: '24h' });
-      return res.json({ message: info.message, token });
-    } catch (error) {
-      return next(error);
-    }
-  })(req, res, next);
-});
+router.post('/login', login);
 
 module.exports = router;
