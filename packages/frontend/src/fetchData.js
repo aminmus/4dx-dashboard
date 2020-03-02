cf68c0f99690422476d6db3327c089005d33729b
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-02 15:14:33 +0100

    Add fetchData module to handle fetching api data

---
+++ packages/frontend/src/fetchData.js
@@ -0,0 +1,30 @@
+const labelResultsWithKey = (data, url) => {
+  const key = url.split('api/')[1];
+  const reformattedResult = {};
+  reformattedResult[key] = data;
+  return reformattedResult;
+};
+
+export default () => {
+  // TODO: replace with ENV variable
+  const baseUrl = 'http://localhost:4000/';
+  const urls = [`${baseUrl}api/nps`, `${baseUrl}api/clients`];
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/vnd.api+json');
+
+  const requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  return Promise.all(
+    urls.map(url =>
+      fetch(url, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          return labelResultsWithKey(result, url);
+        })
+    )
+  );
+};
