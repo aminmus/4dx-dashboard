4aedcf4c1385abcee10359ec4044d17ec61c8683
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-16 18:52:16 +0100

    Fix bug related to Chart instance rendering
    Will now check if instance exists before creating new one
    If instance exists will instead update with new data

--- packages/frontend/src/components/Monitor.js
+++ packages/frontend/src/components/Monitor.js
@@ -1,54 +1,68 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Chart from 'chart.js';
 import PropTypes from 'prop-types';
 
 export default function Monitor(props) {
   const { chart } = props;
   const chartRef = React.createRef();
+  const [graph, setGraph] = useState(null);
+
+  const updateData = (graph, newLabels, newData) => {
+    graph.data.datasets[0].data = newData;
+    graph.data.labels = newLabels;
+    graph.update();
+  };
 
   useEffect(() => {
     const detailsChartRef = chartRef.current.getContext('2d');
     Chart.defaults.global.defaultFontColor = '#7C7C7C';
     Chart.defaults.global.defaultFontSize = 14;
 
-    Chart = new Chart(detailsChartRef, {
-      type: 'line',
-      data: {
-        labels: chart.months,
-        datasets: [
-          {
-            data: chart.values,
-            borderColor: ['rgba(250, 191, 44, 1)'],
-            borderWidth: 3,
-            pointRadius: 5,
-            pointBorderWidth: 3,
-            pointBackgroundColor: 'rgba(250, 191, 44, 1)',
+    if (graph === null) {
+      setGraph(
+        new Chart(detailsChartRef, {
+          type: 'line',
+          data: {
+            labels: chart.months,
+            datasets: [
+              {
+                data: chart.values,
+                borderColor: ['rgba(250, 191, 44, 1)'],
+                borderWidth: 3,
+                pointRadius: 5,
+                pointBorderWidth: 3,
+                pointBackgroundColor: 'rgba(250, 191, 44, 1)',
+                fill: false
+              }
+            ]
+          },
+          options: {
+            scales: {
+              yAxes: [
+                {
+                  scaleLabel: {
+                    fontColor: 'pink'
+                  },
+                  ticks: {
+                    beginAtZero: false,
+                    suggestedMax: 30,
+                    suggestedMin: -60
+                  }
+                }
+              ]
+            },
+            legend: {
+              display: false
+            },
             fill: false
           }
-        ]
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              scaleLabel: {
-                fontColor: 'pink'
-              },
-              ticks: {
-                beginAtZero: false,
-                suggestedMax: 30,
-                suggestedMin: -60
-              }
-            }
-          ]
-        },
-        legend: {
-          display: false
-        },
-        fill: false
-      }
-    });
+        })
+      );
+    } else {
+      updateData(graph, chart.months, chart.values);
+    }
   });
+
   return (
     <div className="chart__nps">
       <canvas id="chart__nps" ref={chartRef} />
