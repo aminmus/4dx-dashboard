c57561ce93689d104322c61ba1f213c223bfc016
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-17 14:57:25 +0100

    :construction: Add CustomLayout, CustomAppBar components
    -Added to handle AppBar overlapping with existing navbar

---
+++ packages/frontend/src/CustomLayout.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { Layout } from 'react-admin';
+import CustomAppBar from './CustomAppBar';
+
+const CustomLayout = props => <Layout {...props} appBar={CustomAppBar} />;
+
+export default CustomLayout;
