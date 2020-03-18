e998c4066b32d4ada8a49e5a3ceb28f771adaf5d
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-18 11:51:28 +0100

    :sparkles: Add form input validation for measures create/edit

--- packages/frontend/src/components/dashboard/measure.js
+++ packages/frontend/src/components/dashboard/measure.js
@@ -1,8 +1,20 @@
 /* eslint-disable camelcase, import/no-extraneous-dependencies, react/destructuring-assignment, react/jsx-props-no-spreading, react/prop-types */
 
 import React from 'react';
-import { Edit, Create, SimpleForm, TextInput, BooleanInput } from 'react-admin';
+import {
+  Edit,
+  Create,
+  SimpleForm,
+  TextInput,
+  BooleanInput,
+  required,
+  minLength,
+  maxLength
+} from 'react-admin';
 import { parse } from 'query-string';
+
+const validateSuccess = [required()];
+const validateDescription = [required(), minLength(5), maxLength(100)];
 
 export const MeasureEdit = props => {
   const { client_id } = parse(props.location.search);
@@ -10,8 +22,8 @@ export const MeasureEdit = props => {
     <Edit title="Edit Client Measure" {...props}>
       <SimpleForm redirect={`/clients/${client_id}/show/measures`}>
         <TextInput disabled source="id" />
-        <TextInput source="description" />
-        <BooleanInput source="success" className="mx-auto" />
+        <TextInput source="description" validate={validateDescription} />
+        <BooleanInput source="success" validate={validateSuccess} className="mx-auto" />
       </SimpleForm>
     </Edit>
   );
@@ -25,8 +37,8 @@ export const MeasureCreate = props => {
         defaultValue={{ ClientId: client_id }}
         redirect={`/clients/${client_id}/show/measures`}
       >
-        <TextInput source="description" />
-        <BooleanInput source="success" className="mx-auto" />
+        <TextInput source="description" validate={validateDescription} />
+        <BooleanInput source="success" validate={validateSuccess} className="mx-auto" />
       </SimpleForm>
     </Create>
   );
