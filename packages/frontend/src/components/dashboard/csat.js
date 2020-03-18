88616f4276dcda838c87424fefb5c53a8883bad1
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-18 11:35:21 +0100

    :sparkles: Add form input validation for csat create/edit

--- packages/frontend/src/components/dashboard/csat.js
+++ packages/frontend/src/components/dashboard/csat.js
@@ -1,8 +1,27 @@
 /* eslint-disable camelcase, import/no-extraneous-dependencies, react/destructuring-assignment, react/jsx-props-no-spreading, react/prop-types */
 
 import React from 'react';
-import { Edit, Create, SimpleForm, TextInput, DateInput, NumberInput } from 'react-admin';
+import {
+  required,
+  minValue,
+  maxValue,
+  number,
+  regex,
+  Edit,
+  Create,
+  SimpleForm,
+  TextInput,
+  DateInput,
+  NumberInput
+} from 'react-admin';
+
 import { parse } from 'query-string';
+
+const validateScore = [required(), number(), minValue(1), maxValue(10)];
+const validateDate = [
+  required(),
+  regex(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/, 'Must be a valid date')
+];
 
 export const CsatEdit = props => {
   const { client_id } = parse(props.location.search);
@@ -10,8 +29,8 @@ export const CsatEdit = props => {
     <Edit title="Edit Client Satisfaction score" {...props}>
       <SimpleForm redirect={`/clients/${client_id}/show/csat`}>
         <TextInput disabled source="id" />
-        <DateInput source="date" />
-        <NumberInput source="score" />
+        <DateInput source="date" validate={validateDate} />
+        <NumberInput source="score" validate={validateScore} />
       </SimpleForm>
     </Edit>
   );
@@ -26,8 +45,8 @@ export const CsatCreate = props => {
         redirect={`/clients/${client_id}/show/csat`}
       >
         <NumberInput source="ClientId" disabled />
-        <DateInput source="date" />
-        <NumberInput source="score" />
+        <DateInput source="date" validate={validateDate} />
+        <NumberInput source="score" validate={validateScore} />
       </SimpleForm>
     </Create>
   );
