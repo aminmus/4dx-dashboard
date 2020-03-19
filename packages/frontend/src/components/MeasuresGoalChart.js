0a4f79624ffbbfc8f10969426ff5895718b99a2f
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-19 20:50:36 +0100

    :art: Restructure and complete MeasuresGoalChart component

--- packages/frontend/src/components/MeasuresGoalChart.js
+++ packages/frontend/src/components/MeasuresGoalChart.js
@@ -2,60 +2,21 @@
 
 import React, { useEffect, useState } from 'react';
 import Chart from 'chart.js';
-import moment from 'moment';
 import formatMeasureProgress from './formatMeasureProgress';
 
 export default function MeasuresGoalChart(props) {
+  const { measures } = props;
   const { targetDate, targetMeasures } = props.measuresGoal;
 
-  formatMeasureProgress();
-
-  // GET THE LATEST 7 WEEKS
-  const measuresDates = [
-    '2020-02-05',
-    '2020-02-12',
-    '2020-02-19',
-    '2020-02-26',
-    '2020-03-04',
-    '2020-03-11',
-    '2020-03-18'
-  ];
-
-  const measuresSuccess = [0, 2, 4, 6, 8, 10, 12];
-
-  // -------------MEASURES COMPLETED LINE GRAPH---------------------//
-  const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
-  const currentDate = new Date().toLocaleDateString('sv-SE', options);
-  const startDate = measuresDates[0];
-
-  const cur = moment(currentDate);
-  const fin = moment(startDate);
-  //   console.log(cur.diff(fin, 'days'));
-  //   console.log(fin);
-  //   const diff =
-
-  // ---------------------------------------------------------------//
+  const formattedMeasureData = formatMeasureProgress(measures, targetDate, targetMeasures);
+  const currentChartLabels = formattedMeasureData.current.map(entry => entry.date);
+  const currentChartValues = formattedMeasureData.current.map(entry => entry.measuresCompleted);
+  const targetChartValues = formattedMeasureData.target.map(entry => {
+    return entry !== null ? entry.measuresCompleted : null;
+  });
 
   const chartRef = React.createRef();
   const [graph, setGraph] = useState(null);
-
-  const updateData = (graph, newLabels, newData) => {
-    graph.data.datasets[0].data = newData;
-    graph.data.labels = newLabels;
-    graph.update();
-  };
-
-  /* CREATE LINEAR DATA SET: 
-  MAX Y : targetMeasures
-  MIN Y : FIRT DATAPOINT IN MEASURES
-  CALCULATE AMOUNT OF INCREMETNS [LENGTH OF ARRAY] : 14 days / 7 days per step
-  
-  AMOUNT OF STEPS:
-  number of days = targetDate - startDate
-  number of array values = 
-
-  LABELS DOES NOT MATTER- THEY WILL MATCH EXISTING DATA
-*/
 
   useEffect(() => {
     const detailsChartRef = chartRef.current.getContext('2d');
@@ -67,12 +28,10 @@ export default function MeasuresGoalChart(props) {
         new Chart(detailsChartRef, {
           type: 'line',
           data: {
-            // TODO: INSERT LABEL DATA FROM MEASURES
-            labels: measuresDates,
+            labels: currentChartLabels,
             datasets: [
               {
-                // TODO: INSERT DATA VALUES FROM MEASURES
-                data: [0, 2, 2, 3, 5, 6],
+                data: currentChartValues,
                 borderColor: ['rgba(250, 191, 44, 1)'],
                 borderWidth: 3,
                 pointRadius: 5,
@@ -81,8 +40,8 @@ export default function MeasuresGoalChart(props) {
                 fill: false
               },
               {
-                // TODO: INSERT DATA VALUES FROM MEASURES
-                data: measuresSuccess,
+                data: targetChartValues,
+                spanGaps: true,
                 borderColor: ['red'],
                 borderWidth: 3,
                 pointRadius: 5,
@@ -101,8 +60,8 @@ export default function MeasuresGoalChart(props) {
                   },
                   ticks: {
                     beginAtZero: false,
-                    suggestedMax: 0,
-                    suggestedMin: 16
+                    suggestedMax: targetMeasures,
+                    suggestedMin: currentChartValues[0]
                   }
                 }
               ]
@@ -114,8 +73,6 @@ export default function MeasuresGoalChart(props) {
           }
         })
       );
-    } else {
-      //   updateData(graph, chart.months, chart.values);
     }
   });
 
