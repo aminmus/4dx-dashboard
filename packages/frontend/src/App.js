92394b68a0a5186dea7a00a439e56350ade4df1e
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-20 12:31:27 +0100

    :fire: Remove redundant initial App State for chart

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -42,8 +42,8 @@ export default function App() {
     implementText: 'Implement Client Success Program for listed clients'
   });
   const [chart, setChart] = useState({
-    months: ['June', 'July', 'August', 'September', 'October', 'November'],
-    values: [0, 0, 0, 0, 0, 0]
+    months: [],
+    values: []
   });
 
   useEffect(() => {
