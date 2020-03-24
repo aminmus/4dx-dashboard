5201c69f0b168ff29078065778cecd3592230fe5
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-24 12:50:13 +0100

    :art: Improve the Measure over time graph

--- packages/frontend/src/components/MeasuresOverTime.js
+++ packages/frontend/src/components/MeasuresOverTime.js
@@ -11,15 +11,13 @@ const updateData = (
   newLabels,
   newData,
   newTargetData,
-  newHighlightData,
   measureLineShow,
   targetLineShow
 ) => {
   graphInstance.data.datasets[0].data = newData;
-  graphInstance.data.datasets[1].data = newHighlightData;
-  graphInstance.data.datasets[2].data = newTargetData;
+  graphInstance.data.datasets[1].data = newTargetData;
   graphInstance.data.datasets[0].showLine = measureLineShow;
-  graphInstance.data.datasets[2].showLine = targetLineShow;
+  graphInstance.data.datasets[1].showLine = targetLineShow;
   graphInstance.data.labels = newLabels;
   graphInstance.update();
 };
@@ -32,41 +30,22 @@ export default function MeasuresOverTime(props) {
   const [measureLineShow, setMeasureLineShow] = useState(true);
   const [targetLineShow, setTargetLineShow] = useState(true);
 
-  const { labels, data } = formatMeasureProgress(measures, targetDate, targetMeasures);
-  const { measuresData, targetData, highlightData } = data;
-
-  const setLastPointRadius = (radiusSize, measures) => {
-    const array = measures.filter(value => value === 0 || value).map(() => 0);
-    array.splice(-1, 1, radiusSize);
-    return array;
-  };
+  const { labels, measuresData, targetData } = formatMeasureProgress(
+    measures,
+    targetDate,
+    targetMeasures
+  );
 
   const toggleMeasures = e => {
     e.preventDefault();
     setMeasureLineShow(!measureLineShow);
-    updateData(
-      graph,
-      labels,
-      measuresData,
-      targetData,
-      highlightData,
-      measureLineShow,
-      targetLineShow
-    );
+    updateData(graph, labels, measuresData, targetData, measureLineShow, targetLineShow);
   };
 
   const toggleTarget = e => {
     e.preventDefault();
     setTargetLineShow(!targetLineShow);
-    updateData(
-      graph,
-      labels,
-      measuresData,
-      targetData,
-      highlightData,
-      measureLineShow,
-      targetLineShow
-    );
+    updateData(graph, labels, measuresData, targetData, measureLineShow, targetLineShow);
   };
 
   useEffect(() => {
@@ -85,23 +64,12 @@ export default function MeasuresOverTime(props) {
                 data: measuresData,
                 borderColor: 'rgba(250, 191, 44, 1)',
                 borderWidth: 2,
-                pointRadius: setLastPointRadius(5, measuresData),
+                pointRadius: 3,
                 pointBorderWidth: 0,
                 pointBackgroundColor: 'rgba(250, 191, 44, 1)',
                 fill: false,
                 steppedLine: true,
                 showLine: measureLineShow
-              },
-              {
-                data: highlightData,
-                borderColor: 'green',
-                borderWidth: 3,
-                pointRadius: 4,
-                pointBorderWidth: 4,
-                pointBackgroundColor: 'green',
-                fill: false,
-                showLine: false,
-                hitRadius: 5
               },
               {
                 data: targetData,
@@ -127,23 +95,16 @@ export default function MeasuresOverTime(props) {
               bodyFontSize: 18,
               titleFontSize: 20,
               callbacks: {
-                title: tooltipItem => {
-                  return tooltipItem[0].label === moment().format('YYYY-MM-DD')
-                    ? `Today (${tooltipItem[0].label})`
-                    : tooltipItem[0].label;
-                },
                 label: (tooltipItem, data) => {
                   const { datasetIndex, value, index } = tooltipItem;
-
-                  if (datasetIndex === 1 || data.labels[index] === moment().format('YYYY-MM-DD')) {
+                  if (datasetIndex === 0) {
                     return `Completed: ${value} Target: ${Math.round(
-                      data.datasets[2].data[index]
+                      data.datasets[1].data[index]
                     )}`;
                   }
-                  if (datasetIndex === 2 && index > 0) {
+                  if (datasetIndex === 1 && index > 0) {
                     return `Target: ${Math.round(value)}`;
                   }
-
                   return null;
                 }
               }
@@ -154,11 +115,8 @@ export default function MeasuresOverTime(props) {
                   ticks: {
                     autoSkip: true,
                     maxTicksLimit: 500,
-                    callback(value, index, values) {
-                      if (index % 7 === 0 || index === values[index.length]) {
-                        return moment(value).format('MMMM DD');
-                      }
-                      return null;
+                    callback(value) {
+                      return moment(value).format('MMMM DD');
                     }
                   }
                 }
@@ -186,15 +144,7 @@ export default function MeasuresOverTime(props) {
         })
       );
     } else {
-      updateData(
-        graph,
-        labels,
-        measuresData,
-        targetData,
-        highlightData,
-        measureLineShow,
-        targetLineShow
-      );
+      updateData(graph, labels, measuresData, targetData, measureLineShow, targetLineShow);
     }
   });
 
--- packages/frontend/src/utils/formatMeasureProgress.js
+++ packages/frontend/src/utils/formatMeasureProgress.js
@@ -1,137 +1,66 @@
 /* eslint-disable no-param-reassign */
 import moment from 'moment';
 
-/*
- Return an array of dates spanning a range of set increments between the current date and target date
-*/
-const calcDateInterval = (currentDate, targetDate, incrementSize) => {
-  const dateInterval = [];
-  let date = targetDate || currentDate;
-  let dateSpan = null;
-
-  if (moment(date).diff(currentDate, 'days') >= 1) {
-    dateSpan = moment(targetDate).diff(currentDate, 'days');
-    for (let i = 0; i <= dateSpan * 2; i += 1) {
-      date = moment(date)
-        .subtract(incrementSize, 'd')
+const setInterval = (startDate = null, targetDate = null) => {
+  const array = [];
+  const start = startDate
+    ? moment(startDate).format('YYYY-MM-DD')
+    : moment()
+        .subtract(2, 'w')
+        .format('YYYY-MM-DD');
+  const end = targetDate
+    ? moment(targetDate).format('YYYY-MM-DD')
+    : moment()
+        .add(2, 'w')
         .format('YYYY-MM-DD');
 
-      dateInterval.push({
-        date,
-        measuresCompleted: null
-      });
-    }
-  } else {
-    dateSpan = 4;
-    for (let i = 0; i <= dateSpan * 2; i += 1) {
-      if (i !== 0) {
-        date = moment(date)
-          .subtract(incrementSize, 'd')
-          .format('YYYY-MM-DD');
-      }
-      dateInterval.push({
-        date: moment(date).format('YYYY-MM-DD'),
-        measuresCompleted: null
-      });
-    }
+  const span = moment(end).diff(start, 'w');
+  for (let i = 0; i <= span; i += 1) {
+    array.push(
+      moment(start)
+        .add(i, 'w')
+        .format('YYYY-MM-DD')
+    );
   }
-  return dateInterval;
+  return array;
 };
 
-/*
- Populate date array with successful measures matching the date.
-*/
-const populateMeasuresCompleted = (measures, dates, currentDate) => {
-  // Run through all of the successful measures
-  measures.forEach(successfulMeasureDate => {
-    // For each entry check with the current dateInterval dates
-    dates.forEach(dateIntervalEntry => {
-      // Setting up the initial count for succesful measures
-      if (
-        !dateIntervalEntry.measuresCompleted &&
-        moment(dateIntervalEntry.date).isSameOrBefore(currentDate)
-      ) {
-        dateIntervalEntry.measuresCompleted = 0;
-      }
-      // Add to the count if succesful measure occurs before dateInterval date
-      // but before/on the current date
-      if (
-        moment(dateIntervalEntry.date).isSameOrAfter(successfulMeasureDate) &&
-        moment(dateIntervalEntry.date).isSameOrBefore(currentDate)
-      ) {
-        dateIntervalEntry.measuresCompleted += 1;
-      }
-    });
-  });
-
-  return dates.reverse();
+const setDataPointsMeasure = (interval = null, measures = null) => {
+  return !measures || !interval
+    ? []
+    : interval.reduce((dataPointArray, date) => {
+        let i = 0;
+        measures.forEach(measureDate => {
+          if (moment(date).isSameOrAfter(measureDate)) {
+            i += 1;
+          }
+        });
+        dataPointArray.push(i);
+        return dataPointArray;
+      }, []);
 };
 
-/*
- Return array with start/end values for measures, populate interspersing data with null values
- for ChartJS interpolation
-*/
-const calcTargetData = (dateMeasureArray, targetDate, targetMeasures) => {
-  const differenceX = moment(targetDate).diff(dateMeasureArray[0].date, 'd');
-  const differenceY = targetMeasures - dateMeasureArray[0].measuresCompleted;
-  const slope = differenceY / differenceX;
-
-  const target = [];
-
-  if (targetDate || targetMeasures) {
-    for (let k = 0; k < dateMeasureArray.length; k += 1) {
-      target.push({
-        date: dateMeasureArray[k].date,
-        measuresCompleted:
-          moment(dateMeasureArray[k].date).diff(dateMeasureArray[0].date, 'd') * slope
-      });
+const setDataPointsTarget = (data = null, targetMeasures = null, targetDate = null) => {
+  if (data || targetMeasures || targetDate) {
+    const targetArray = [];
+    const yDelta = targetMeasures - data[0];
+    const xDelta = data.length - 1;
+    const slope = yDelta / xDelta;
+    for (let i = 0; i <= xDelta; i += 1) {
+      targetArray.push(i * slope);
     }
+    return targetArray;
   }
-  return target;
-};
-
-const filterDatesAfterCurrentDate = dataSet => {
-  return dataSet.filter(dataPoint => moment(dataPoint.date).isSameOrBefore(moment()));
-};
-
-const highlightDataChanges = dataSet => {
-  const highlights = [];
-  highlights.push(dataSet[0].measuresCompleted);
-  for (let i = 1; i < dataSet.length; i += 1) {
-    if (dataSet[i].measuresCompleted !== dataSet[i - 1].measuresCompleted) {
-      highlights.push(dataSet[i].measuresCompleted);
-    } else {
-      highlights.push(null);
-    }
-  }
-  return highlights;
+  return [];
 };
 
 export default function(measures, targetDate, targetMeasures) {
-  const successfulMeasures = measures.filter(dateOfSuccess => dateOfSuccess);
-
-  const currentDate = moment();
-
-  const dateInterval = calcDateInterval(currentDate, targetDate, 1);
-
-  const dateIntervalWithMeasures = populateMeasuresCompleted(
-    successfulMeasures,
-    dateInterval,
-    currentDate
-  );
-
-  const targetArray = calcTargetData(dateIntervalWithMeasures, targetDate, targetMeasures);
-
-  const filteredArray = filterDatesAfterCurrentDate(dateIntervalWithMeasures);
-
-  const highlightedDataSet = highlightDataChanges(filteredArray);
-
+  const interval = setInterval(null, targetDate);
+  const datapointsMeasure = setDataPointsMeasure(interval, measures);
+  const dataPointsTarget = setDataPointsTarget(datapointsMeasure, targetMeasures, targetDate);
   return {
-    data: {
-      measuresData: dateIntervalWithMeasures.map(entry => entry.measuresCompleted),
-      targetData: targetArray.map(entry => (entry ? entry.measuresCompleted : null)),
-      highlightData: highlightedDataSet
-    },
-    labels: dateIntervalWithMeasures.map(entry => entry.date)
+    measuresData: datapointsMeasure,
+    targetData: dataPointsTarget,
+    labels: interval
   };
 }
