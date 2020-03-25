3ef51e1127106758fdaca4f13af5baf5718293fa
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-25 15:08:04 +0100

    :lipstick: Fix progressbar UI and responsive behaviour

--- packages/frontend/src/components/ClientDetails.js
+++ packages/frontend/src/components/ClientDetails.js
@@ -7,17 +7,47 @@ import MeasureCheckList from './admin/MeasureCheckList';
 export default function ClientDetails(props) {
   const { client } = props;
   const [renderChecklist, setRenderChecklist] = useState(false);
+  const [hoverState, setHoverState] = useState(false);
+
+  const ContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    margin: '20px 0',
+    cursor: hoverState ? 'pointer' : 'auto'
+  };
+
+  const MeasureCheckListContainerStyle = {
+    display: 'flex',
+    justifyContent: 'darkGray',
+    border: '1px solid white',
+    borderRadius: '10px',
+    margin: '0, 10px 0 10px'
+  };
 
   const handleToggleClick = e => {
     e.preventDefault();
     setRenderChecklist(!renderChecklist);
   };
 
+  const toggleHoverState = e => {
+    e.preventDefault();
+    setHoverState(!hoverState);
+  };
+
   return (
-    <div onClick={handleToggleClick} onKeyDown={handleToggleClick}>
+    <div
+      onMouseEnter={toggleHoverState}
+      onMouseLeave={toggleHoverState}
+      onClick={handleToggleClick}
+      onKeyDown={handleToggleClick}
+      style={ContainerStyle}
+    >
       <Progressbar key={client.id} clientName={client.name} clientScore={client.progress} />
       {renderChecklist && client.measures.length > 0 && (
-        <MeasureCheckList key={`${client.id}-checklist`} measures={client.measures} />
+        <div style={MeasureCheckListContainerStyle}>
+          <MeasureCheckList key={`${client.id}-checklist`} measures={client.measures} />
+        </div>
       )}
     </div>
   );
