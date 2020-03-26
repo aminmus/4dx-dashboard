3c541224637d825ea40d0fbebda3dfcb1d7e212b
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-26 15:55:59 +0100

    :lipstick: Add color variables

---
+++ packages/frontend/src/style/COLORS.js
@@ -0,0 +1,8 @@
+const COLORS = {
+  primary: 'rgba(250, 191, 44, 1)',
+  secondary: 'rgba(200, 140, 10, 1)',
+  light: 'rgba(255, 255, 255, 1)',
+  dark: 'rgba(0, 0, 0 , 1)'
+};
+
+export default COLORS;
