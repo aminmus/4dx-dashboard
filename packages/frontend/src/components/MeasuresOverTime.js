71a8c259201999c60ed2908cc5abe48dcc358d76
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-26 16:49:58 +0100

    :art: Refactor component code and implement color variables

--- packages/frontend/src/components/MeasuresOverTime.js
+++ packages/frontend/src/components/MeasuresOverTime.js
@@ -1,20 +1,19 @@
-/* eslint-disable no-unused-vars, react/prop-types, no-param-reassign, react/jsx-one-expression-per-line, no-shadow, no-unused-expressions */
-
 import React, { useEffect, useState, useRef } from 'react';
 import Chart from 'chart.js';
 import moment from 'moment';
-import Button from '@material-ui/core/Button';
-import DehazeIcon from '@material-ui/icons/Dehaze';
-import { IconButton } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import formatMeasureProgress from '../utils/formatMeasureProgress';
+import OptionsToggleButton from './elements/OptionsToggleButton';
+import OptionsButton from './elements/OptionsButton';
+import COLORS from '../style/COLORS';
 
-/* -------------------------------  STYLES ------------------------------- */
+const { primary, light, danger, gray } = COLORS;
+
 const ChartHeaderStyle = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  color: 'rgba(250, 191, 44, 1)'
+  color: primary
 };
 
 const ContainerStyle = {
@@ -23,61 +22,19 @@ const ContainerStyle = {
 
 const OptionsContainerStyle = {
   margin: '10px',
-  border: '1px solid white',
+  border: `1px solid ${light}`,
   borderRadius: '10px'
 };
-const OptionsButton = withStyles({
-  label: {
-    color: 'black'
-  },
-  root: {
-    backgroundColor: 'rgba(250, 191, 44, 1)',
-    margin: '10px',
-    '&:hover': {
-      backgroundColor: 'rgba(200, 140, 10, 1)',
-      borderColor: '#0062cc',
-      boxShadow: 'none'
-    }
-  }
-})(Button);
-
-const OptionsToggleButton = withStyles({
-  root: {
-    color: 'rgba(250, 191, 44, 1)',
-    margin: '10px',
-    '&:hover': {
-      backgroundColor: 'rgba(200, 140, 10, 1)',
-      borderColor: '#0062cc',
-      boxShadow: 'none'
-    },
-    '&:focus': {
-      outline: 'none'
-    }
-  }
-})(IconButton);
-
-const OptionsToggleIcon = withStyles({
-  root: {
-    color: 'rgba(250, 191, 44, 1)',
-    '&:hover': {
-      color: 'white'
-    }
-  }
-})(DehazeIcon);
-
-/* ------------------------------------------------------------------ */
 
 const MeasuresOverTime = props => {
   const { measures, measuresGoal } = props;
   const { targetDate, targetMeasures } = measuresGoal;
 
-  // GET DATA -------------------------------------------
   const { labels, measuresData, targetData } = formatMeasureProgress(
     measures,
     targetDate,
     targetMeasures
   );
-  /* --------------------------------------------------------------------- */
 
   const [measureDatasetData, setMeasureDatasetData] = useState(measuresData);
   const [targetDatasetData, setTargetDatasetData] = useState(targetData);
@@ -96,6 +53,7 @@ const MeasuresOverTime = props => {
     e.preventDefault();
     setOptionsShow(!optionsShow);
   };
+
   const optionsToggleMeasures = e => {
     e.preventDefault();
     setDisplayMeasures(!displayMeasures);
@@ -116,11 +74,11 @@ const MeasuresOverTime = props => {
       datasets: [
         {
           data: measureDatasetData,
-          borderColor: 'rgba(250, 191, 44, 1)',
+          borderColor: primary,
           borderWidth: 2,
           pointRadius: 3,
           pointBorderWidth: 0,
-          pointBackgroundColor: 'rgba(250, 191, 44, 1)',
+          pointBackgroundColor: primary,
           fill: false,
           showLine: displayMeasures,
           lineTension: smoothLine ? 0.4 : 0
@@ -128,11 +86,11 @@ const MeasuresOverTime = props => {
         {
           data: targetDatasetData,
           spanGaps: true,
-          borderColor: ['red'],
+          borderColor: danger,
           borderWidth: 0,
           pointRadius: 0,
           pointBorderWidth: 0,
-          pointBackgroundColor: 'red',
+          pointBackgroundColor: danger,
           showLine: displayTarget,
           fill: false
         }
@@ -173,7 +131,7 @@ const MeasuresOverTime = props => {
             scaleLabel: {
               display: true,
               labelString: 'Measures Completed',
-              fontColor: 'rgba(250, 191, 44, 1)'
+              fontColor: primary
             },
             ticks: {
               beginAtZero: false,
@@ -191,7 +149,7 @@ const MeasuresOverTime = props => {
   };
 
   useEffect(() => {
-    Chart.defaults.global.defaultFontColor = '#7C7C7C';
+    Chart.defaults.global.defaultFontColor = gray;
     Chart.defaults.global.defaultFontSize = 14;
     setMeasureDatasetData(measuresData);
     setTargetDatasetData(targetData);
@@ -205,52 +163,60 @@ const MeasuresOverTime = props => {
       chartInstance.data.datasets[1].data = targetDatasetData;
       chartInstance.data.labels = labelsData;
     }
-  }, [chartContainer, displayMeasures, displayTarget, smoothLine]);
+  }, [
+    chartContainer,
+    displayMeasures,
+    displayTarget,
+    smoothLine,
+    measures,
+    measuresGoal,
+    targetDate,
+    targetMeasures
+  ]);
 
   return (
     <div style={ContainerStyle}>
       <div style={ChartHeaderStyle}>
         <div className="chart-title">Measures Over Time (Weekly)</div>
-        <OptionsToggleButton
-          aria-label="options"
-          style={{ marginLeft: '10px' }}
-          onClick={toggleOptions}
-        >
-          <OptionsToggleIcon />
-        </OptionsToggleButton>
+        <div>
+          <OptionsToggleButton onClick={toggleOptions} />
+        </div>
       </div>
       {optionsShow && (
         <div style={OptionsContainerStyle}>
           <OptionsButton
-            color="default"
-            size="small"
-            variant="contained"
+            text={smoothLine ? 'Set Tense' : 'Set Smooth'}
             onClick={optionsToggleSmooth}
-          >
-            {smoothLine ? 'Set Tense' : 'Set Smooth'}
-          </OptionsButton>
+          />
           <OptionsButton
-            color="default"
-            size="small"
-            variant="contained"
+            text={displayMeasures ? 'Hide Measures' : 'Show Measures'}
             onClick={optionsToggleMeasures}
-          >
-            {displayMeasures ? 'Hide Measures' : 'Show Measures'}
-          </OptionsButton>
+          />
           <OptionsButton
-            color="default"
-            size="small"
-            variant="contained"
+            text={displayTarget ? 'Hide Target' : 'Show Target'}
             onClick={optionsToggleTarget}
-          >
-            {displayTarget ? 'Hide Target' : 'Show Target'}
-          </OptionsButton>
+          />
         </div>
       )}
-
       <canvas ref={chartContainer} />
     </div>
   );
+};
+
+MeasuresOverTime.defaultProps = {
+  measures: [],
+  measuresGoal: {
+    targetDate: '',
+    targetMeasures: 0
+  }
+};
+
+MeasuresOverTime.propTypes = {
+  measures: PropTypes.arrayOf(PropTypes.string),
+  measuresGoal: PropTypes.shape({
+    targetDate: PropTypes.string,
+    targetMeasures: PropTypes.number
+  })
 };
 
 export default MeasuresOverTime;
