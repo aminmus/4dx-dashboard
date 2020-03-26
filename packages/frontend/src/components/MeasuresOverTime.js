bd3f89527c50d54759cbf13631ae9358deb44c08
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-26 13:49:53 +0100

    :lipstick: :art: Update UI of Measure Over Time Graph

--- packages/frontend/src/components/MeasuresOverTime.js
+++ packages/frontend/src/components/MeasuresOverTime.js
@@ -1,220 +1,256 @@
 /* eslint-disable no-unused-vars, react/prop-types, no-param-reassign, react/jsx-one-expression-per-line, no-shadow, no-unused-expressions */
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Chart from 'chart.js';
 import moment from 'moment';
-import { Button } from 'react-bootstrap';
+import Button from '@material-ui/core/Button';
+import DehazeIcon from '@material-ui/icons/Dehaze';
+import { IconButton } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import formatMeasureProgress from '../utils/formatMeasureProgress';
 
-const updateData = (
-  graphInstance,
-  newLabels,
-  newData,
-  newTargetData,
-  measureLineShow,
-  targetLineShow,
-  lineTension
-) => {
-  graphInstance.data.datasets[0].data = newData;
-  graphInstance.data.datasets[1].data = newTargetData;
-  graphInstance.data.datasets[0].showLine = measureLineShow;
-  graphInstance.data.datasets[1].showLine = targetLineShow;
-  graphInstance.data.labels = newLabels;
-  graphInstance.data.datasets[0].lineTension = lineTension;
-  graphInstance.update();
+/* -------------------------------  STYLES ------------------------------- */
+const ChartHeaderStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: 'rgba(250, 191, 44, 1)'
 };
 
-export default function MeasuresOverTime(props) {
+const ContainerStyle = {
+  margin: '10px'
+};
+
+const OptionsContainerStyle = {
+  margin: '10px',
+  border: '1px solid white',
+  borderRadius: '10px'
+};
+const OptionsButton = withStyles({
+  label: {
+    color: 'black'
+  },
+  root: {
+    backgroundColor: 'rgba(250, 191, 44, 1)',
+    margin: '10px',
+    '&:hover': {
+      backgroundColor: 'rgba(200, 140, 10, 1)',
+      borderColor: '#0062cc',
+      boxShadow: 'none'
+    }
+  }
+})(Button);
+
+const OptionsToggleButton = withStyles({
+  root: {
+    color: 'rgba(250, 191, 44, 1)',
+    margin: '10px',
+    '&:hover': {
+      backgroundColor: 'rgba(200, 140, 10, 1)',
+      borderColor: '#0062cc',
+      boxShadow: 'none'
+    },
+    '&:focus': {
+      outline: 'none'
+    }
+  }
+})(IconButton);
+
+const OptionsToggleIcon = withStyles({
+  root: {
+    color: 'rgba(250, 191, 44, 1)',
+    '&:hover': {
+      color: 'white'
+    }
+  }
+})(DehazeIcon);
+
+/* ------------------------------------------------------------------ */
+
+const MeasuresOverTime = props => {
   const { measures, measuresGoal } = props;
   const { targetDate, targetMeasures } = measuresGoal;
-  const chartRef = React.createRef();
-  const [graph, setGraph] = useState(null);
-  const [measureLineShow, setMeasureLineShow] = useState(true);
-  const [targetLineShow, setTargetLineShow] = useState(true);
-  const [lineTension, setLineTension] = useState(0);
 
+  // GET DATA -------------------------------------------
   const { labels, measuresData, targetData } = formatMeasureProgress(
     measures,
     targetDate,
     targetMeasures
   );
+  /* --------------------------------------------------------------------- */
 
-  const toggleMeasures = e => {
+  const [measureDatasetData, setMeasureDatasetData] = useState(measuresData);
+  const [targetDatasetData, setTargetDatasetData] = useState(targetData);
+  const [labelsData, setLabelsData] = useState(labels);
+
+  const [displayMeasures, setDisplayMeasures] = useState(true);
+  const [displayTarget, setDisplayTarget] = useState(true);
+  const [smoothLine, setSmoothLine] = useState(true);
+
+  const [optionsShow, setOptionsShow] = useState(false);
+
+  const chartContainer = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
+
+  const toggleOptions = e => {
     e.preventDefault();
-    setMeasureLineShow(!measureLineShow);
-    updateData(
-      graph,
-      labels,
-      measuresData,
-      targetData,
-      measureLineShow,
-      targetLineShow,
-      lineTension
-    );
+    setOptionsShow(!optionsShow);
+  };
+  const optionsToggleMeasures = e => {
+    e.preventDefault();
+    setDisplayMeasures(!displayMeasures);
+  };
+  const optionsToggleTarget = e => {
+    e.preventDefault();
+    setDisplayTarget(!displayTarget);
+  };
+  const optionsToggleSmooth = e => {
+    e.preventDefault();
+    setSmoothLine(!smoothLine);
   };
 
-  const toggleTarget = e => {
-    e.preventDefault();
-    setTargetLineShow(!targetLineShow);
-    updateData(
-      graph,
+  const chartConfig = {
+    type: 'line',
+    data: {
       labels,
-      measuresData,
-      targetData,
-      measureLineShow,
-      targetLineShow,
-      lineTension
-    );
-  };
-
-  const toggleLineTension = e => {
-    e.preventDefault();
-    setLineTension(lineTension === 0.4 ? 0 : 0.4);
-    updateData(
-      graph,
-      labels,
-      measuresData,
-      targetData,
-      measureLineShow,
-      targetLineShow,
-      lineTension
-    );
-  };
-
-  const filterRenderByDate = (measuresData, labels) => {
-    return measuresData;
+      datasets: [
+        {
+          data: measureDatasetData,
+          borderColor: 'rgba(250, 191, 44, 1)',
+          borderWidth: 2,
+          pointRadius: 3,
+          pointBorderWidth: 0,
+          pointBackgroundColor: 'rgba(250, 191, 44, 1)',
+          fill: false,
+          showLine: displayMeasures,
+          lineTension: smoothLine ? 0.4 : 0
+        },
+        {
+          data: targetDatasetData,
+          spanGaps: true,
+          borderColor: ['red'],
+          borderWidth: 0,
+          pointRadius: 0,
+          pointBorderWidth: 0,
+          pointBackgroundColor: 'red',
+          showLine: displayTarget,
+          fill: false
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      tooltips: {
+        bodyFontSize: 18,
+        titleFontSize: 20,
+        callbacks: {
+          label: (tooltipItem, data) => {
+            const { datasetIndex, value, index } = tooltipItem;
+            if (datasetIndex === 0) {
+              return `Completed: ${value} Target: ${Math.round(data.datasets[1].data[index])}`;
+            }
+            if (datasetIndex === 1 && index > 0) {
+              return `Target: ${Math.round(value)}`;
+            }
+            return null;
+          }
+        }
+      },
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              autoSkip: true,
+              maxTicksLimit: 500,
+              callback(value) {
+                return moment(value).format('MMMM DD');
+              }
+            }
+          }
+        ],
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: 'Measures Completed',
+              fontColor: 'rgba(250, 191, 44, 1)'
+            },
+            ticks: {
+              beginAtZero: false,
+              suggestedMax: targetMeasures,
+              suggestedMin: measuresData[0]
+            }
+          }
+        ]
+      },
+      legend: {
+        display: false
+      },
+      fill: false
+    }
   };
 
   useEffect(() => {
-    const detailsChartRef = chartRef.current.getContext('2d');
     Chart.defaults.global.defaultFontColor = '#7C7C7C';
     Chart.defaults.global.defaultFontSize = 14;
-
-    if (!graph) {
-      setGraph(
-        new Chart(detailsChartRef, {
-          type: 'line',
-          data: {
-            labels,
-            datasets: [
-              {
-                data: filterRenderByDate(measuresData, labels),
-                borderColor: 'rgba(250, 191, 44, 1)',
-                borderWidth: 2,
-                pointRadius: 3,
-                pointBorderWidth: 0,
-                pointBackgroundColor: 'rgba(250, 191, 44, 1)',
-                fill: false,
-                showLine: measureLineShow,
-                lineTension: 0
-              },
-              {
-                data: targetData,
-                spanGaps: true,
-                borderColor: ['red'],
-                borderWidth: 0,
-                pointRadius: 0,
-                pointBorderWidth: 0,
-                pointBackgroundColor: 'red',
-                showLine: targetLineShow,
-                fill: false
-              }
-            ]
-          },
-          options: {
-            responsive: true,
-            title: {
-              display: true,
-              text: 'Measures Completed Over Time (weekly)',
-              fontColor: 'rgba(250, 191, 44, 1)'
-            },
-            tooltips: {
-              bodyFontSize: 18,
-              titleFontSize: 20,
-              callbacks: {
-                label: (tooltipItem, data) => {
-                  const { datasetIndex, value, index } = tooltipItem;
-                  if (datasetIndex === 0) {
-                    return `Completed: ${value} Target: ${Math.round(
-                      data.datasets[1].data[index]
-                    )}`;
-                  }
-                  if (datasetIndex === 1 && index > 0) {
-                    return `Target: ${Math.round(value)}`;
-                  }
-                  return null;
-                }
-              }
-            },
-            scales: {
-              xAxes: [
-                {
-                  ticks: {
-                    autoSkip: true,
-                    maxTicksLimit: 500,
-                    callback(value) {
-                      return moment(value).format('MMMM DD');
-                    }
-                  }
-                }
-              ],
-              yAxes: [
-                {
-                  scaleLabel: {
-                    display: true,
-                    labelString: 'Measures Completed',
-                    fontColor: 'rgba(250, 191, 44, 1)'
-                  },
-                  ticks: {
-                    beginAtZero: false,
-                    suggestedMax: targetMeasures,
-                    suggestedMin: measuresData[0]
-                  }
-                }
-              ]
-            },
-            legend: {
-              display: false
-            },
-            fill: false
-          }
-        })
-      );
-    } else {
-      updateData(
-        graph,
-        labels,
-        measuresData,
-        targetData,
-        measureLineShow,
-        targetLineShow,
-        lineTension
-      );
+    setMeasureDatasetData(measuresData);
+    setTargetDatasetData(targetData);
+    setLabelsData(labels);
+    if (chartContainer && chartContainer.current) {
+      const newChartInstance = new Chart(chartContainer.current, chartConfig);
+      setChartInstance(newChartInstance);
     }
-  }, [
-    chartRef,
-    graph,
-    labels,
-    measuresData,
-    measureLineShow,
-    targetData,
-    targetLineShow,
-    targetMeasures,
-    lineTension
-  ]);
+    if (chartInstance) {
+      chartInstance.data.datasets[0].data = measureDatasetData;
+      chartInstance.data.datasets[1].data = targetDatasetData;
+      chartInstance.data.labels = labelsData;
+    }
+  }, [chartContainer, displayMeasures, displayTarget, smoothLine]);
 
   return (
-    <div className="chart__nps">
-      <canvas id="chart__nps" ref={chartRef} />
-      <Button size="sm" onClick={toggleMeasures} variant="warning">
-        Toggle Measure Graph
-      </Button>{' '}
-      <Button size="sm" onClick={toggleTarget} variant="warning">
-        Toggle Target Graph
-      </Button>{' '}
-      <Button size="sm" onClick={toggleLineTension} variant="warning">
-        Toggle Line Tension
-      </Button>
+    <div style={ContainerStyle}>
+      <div style={ChartHeaderStyle}>
+        <div className="chart-title">Measures Over Time (Weekly)</div>
+        <OptionsToggleButton
+          aria-label="options"
+          style={{ marginLeft: '10px' }}
+          onClick={toggleOptions}
+        >
+          <OptionsToggleIcon />
+        </OptionsToggleButton>
+      </div>
+      {optionsShow && (
+        <div style={OptionsContainerStyle}>
+          <OptionsButton
+            color="default"
+            size="small"
+            variant="contained"
+            onClick={optionsToggleSmooth}
+          >
+            {smoothLine ? 'Set Tense' : 'Set Smooth'}
+          </OptionsButton>
+          <OptionsButton
+            color="default"
+            size="small"
+            variant="contained"
+            onClick={optionsToggleMeasures}
+          >
+            {displayMeasures ? 'Hide Measures' : 'Show Measures'}
+          </OptionsButton>
+          <OptionsButton
+            color="default"
+            size="small"
+            variant="contained"
+            onClick={optionsToggleTarget}
+          >
+            {displayTarget ? 'Hide Target' : 'Show Target'}
+          </OptionsButton>
+        </div>
+      )}
+
+      <canvas ref={chartContainer} />
     </div>
   );
-}
+};
+
+export default MeasuresOverTime;
