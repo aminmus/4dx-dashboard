2babe31e64802859d7c5818e3e15fa7480a48839
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-31 16:11:58 +0200

    :construction: Add migration to update User table with role

---
+++ packages/backend/db/migrations/20200331132707-user-role.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.addColumn('Users', 'role', {
+			type: Sequelize.STRING,
+			allowNull: false
+		});
+	},
+
+	down: (queryInterface, _Sequelize) => {
+		return queryInterface.removeColumn('Users', 'role');
+	}
+};
