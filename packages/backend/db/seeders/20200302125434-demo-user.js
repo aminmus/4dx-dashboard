e707f3a4905052600d277fe8cfd9d5e64120cdc2
Daniel Salin <daniel.salin@chasacademy.com>
2020-03-31 16:12:25 +0200

    :construction: Update seeded user with admin role

--- packages/backend/db/seeders/20200302125434-demo-user.js
+++ packages/backend/db/seeders/20200302125434-demo-user.js
@@ -2,14 +2,16 @@
 const bcrypt = require('bcryptjs');
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('Users',
-    [{
-      email: 'admin@example.com',
-      password: await bcrypt.hash('admin', 10),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }]),
-  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Users',
-    { email: 'admin@example.com' },
-    {}),
+	up: async (queryInterface, Sequelize) =>
+		queryInterface.bulkInsert('Users', [
+			{
+				email: 'admin@example.com',
+				password: await bcrypt.hash('admin', 10),
+				role: 'admin',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			}
+		]),
+	down: (queryInterface, Sequelize) =>
+		queryInterface.bulkDelete('Users', { email: 'admin@example.com' }, {})
 };
