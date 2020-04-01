34dfcbf062b963af76d8c66fb21b72abb8c8c0b2
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-01 12:50:49 +0200

    :sparkles: Add permissions middleware with role check

---
+++ packages/backend/src/middleware/permissions.js
@@ -0,0 +1,22 @@
+const ROLES = require('../utils/roles');
+
+const canEdit = async (req, res, next) => {
+  const { role } = await req.user;
+
+  if (!role) {
+    return res.status(404).json({
+      error: { title: 'User has no role' },
+      data: null,
+    });
+  }
+  if (role !== ROLES.Admin) {
+    return res.status(404).json({
+      error: { title: 'Invalid user permission' },
+      data: null,
+    });
+  }
+
+  return next();
+};
+
+module.exports = { canEdit };
