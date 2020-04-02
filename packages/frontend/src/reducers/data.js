a11bec4ef49e2643375b3eb21c3a6331e6caa8ca
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-02 17:28:03 +0200

    :construction: Add data reducer and action creator

---
+++ packages/frontend/src/reducers/data.js
@@ -0,0 +1,10 @@
+import initialState from '../initialDashboardState';
+
+const dashboard = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default dashboard;
