d33a736ed7a537f7a45621388925133f82aa4f3f
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-06 16:58:41 +0200

    :construction: WIP create and export store from index

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -2,12 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createHashHistory } from 'history';
 import App from './App';
+import createReduxStore from './createReduxStore';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import authProvider from './utils/react-admin/authProvider';
+import dataProvider from './utils/react-admin/dataProvider';
+
+const history = createHashHistory();
+
+const store = createReduxStore({
+  authProvider,
+  dataProvider,
+  history
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App history={history} />
+  </Provider>,
+
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+export default store;
