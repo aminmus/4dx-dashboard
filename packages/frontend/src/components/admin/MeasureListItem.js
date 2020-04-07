d32c15a5bb53fe24884b13c3db7d2b48cbc576a5
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-07 17:30:56 +0200

    :construction: WIP add InputMeasure to MeasureListItem

--- packages/frontend/src/components/admin/MeasureListItem.js
+++ packages/frontend/src/components/admin/MeasureListItem.js
@@ -1,15 +1,17 @@
 /* eslint-disable no-shadow, no-console */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { ListItem } from '@material-ui/core';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import CancelIcon from '@material-ui/icons/Cancel';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import EditButton from '../elements/EditButton';
+import InputMeasure from '../elements/editMode/InputMeasure';
 
 const MeasureListItem = props => {
   const { measure, editMode } = props;
+  const [isEditingMeasure, setIsEditingMeasure] = useState(true);
 
   const onClickEdit = e => {
     e.preventDefault();
@@ -18,15 +20,21 @@ const MeasureListItem = props => {
 
   return (
     <ListItem className="text-light">
-      <div>
-        {measure.success ? (
-          <CheckCircleIcon className="mr-2 text-success" />
-        ) : (
-          <CancelIcon className="mr-2 text-danger" />
-        )}
-        {measure.description}
-        {editMode && <EditButton onClick={onClickEdit} />}
-      </div>
+      {!isEditingMeasure ? (
+        <div>
+          {measure.success ? (
+            <CheckCircleIcon className="mr-2 text-success" />
+          ) : (
+            <CancelIcon className="mr-2 text-danger" />
+          )}
+          {measure.description}
+          {editMode && <EditButton onClick={onClickEdit} />}
+        </div>
+      ) : (
+        <div>
+          <InputMeasure measure={measure} setIsEditingMeasure={setIsEditingMeasure} />
+        </div>
+      )}
     </ListItem>
   );
 };
