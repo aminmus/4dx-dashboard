282085f8a9b6b235ce89ee50c7eeb4ca0c341f6f
Amin Mushehid <amin.mushehid@vaimo.com>
2020-04-07 21:35:50 +0200

    Add resources action creator and reducer.

--- packages/frontend/src/reducers/resources.js
+++ packages/frontend/src/reducers/resources.js
@@ -1,5 +1,10 @@
+import { GET_RESOURCES } from '../actions/types';
+
 const resourcesReducer = (state = null, action) => {
   switch (action.type) {
+    case GET_RESOURCES:
+      console.log('GET_RESOURCES ACTION SENT');
+      return action.payload;
     default:
       return state;
   }
