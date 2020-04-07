df77087026c517a797b1e7f737f955a7a5fadfd0
Amin Mushehid <amin.mushehid@vaimo.com>
2020-04-07 22:43:57 +0200

    Add descriptive comment

--- packages/frontend/src/initialState.js
+++ packages/frontend/src/initialState.js
@@ -7,6 +7,7 @@ const editMode = {
   editModeEnabled: false
 };
 
+// Other state that is generated client side
 const computed = {
   definedStatus: { totalClients: 0, definedClients: 0 },
   leadStatus: { leads: 0, leadsTotal: 0 },
