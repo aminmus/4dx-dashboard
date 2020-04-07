0a37d42afc0b8e87dbd35e5be74d0e0fe0e20707
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-07 11:11:46 +0200

    :lisptick: Add EditButton element

---
+++ packages/frontend/src/components/elements/EditButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import EditIcon from '@material-ui/icons/Edit';
+import { IconButton } from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+const EditButton = props => {
+  const { onClick } = props;
+
+  return (
+    <IconButton onClick={onClick} aria-label="edit" style={{ marginLeft: '10px' }}>
+      <EditIcon />
+    </IconButton>
+  );
+};
+
+EditButton.defaultProps = {};
+
+EditButton.propTypes = {
+  onClick: PropTypes.func.isRequired
+};
+
+export default EditButton;
