484d3e3658a55182c9c4a060d947960bddbc772c
Amin Mushehid <amin.mushehid@vaimo.com>
2020-04-09 01:16:08 +0200

    Reformat resources data before setting it in redux store
    Also use jsonapi-serializer to deserialize nps data

--- packages/frontend/src/actions/resources.js
+++ packages/frontend/src/actions/resources.js
@@ -1,13 +1,36 @@
-import fetchData from '../utils/fetchData';
+import { Deserializer } from 'jsonapi-serializer';
+
 import { GET_RESOURCES } from './types';
+import fetchData from '../utils/fetchData';
+import reformatClientData from '../utils/reformatClientData';
+import reformatMeasureGoals from '../utils/reformatMeasureGoals';
+import reformatMeasures from '../utils/reformatMeasures';
+
+const deserializer = new Deserializer({
+  keyForAttribute: 'camelCase'
+});
 
 export default function fetchResources() {
   return async function fetchResourcesThunk(dispatch) {
     try {
-      const data = await fetchData();
+      const {
+        nps,
+        clients: fetchedClients,
+        measures: { data: measuresData },
+        measureGoals: { data: measureGoalsData }
+      } = await fetchData();
+
+      const formattedData = {
+        clients: reformatClientData(fetchedClients),
+        nps: await deserializer.deserialize(nps),
+        measures: reformatMeasures(measuresData),
+        measureGoals: reformatMeasureGoals(measureGoalsData)
+      };
+      console.log(formattedData);
+
       return dispatch({
         type: GET_RESOURCES,
-        payload: data
+        payload: formattedData
       });
     } catch (error) {
       console.error(error);
