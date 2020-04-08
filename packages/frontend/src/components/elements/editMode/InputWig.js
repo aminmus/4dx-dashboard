1a9d3a0eb2008922bd1d688c9bb3d4102a4b5fdd
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-08 12:49:05 +0200

    :construction: Add InputWig editMode element

---
+++ packages/frontend/src/components/elements/editMode/InputWig.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
+import moment from 'moment';
+import { TextField } from '@material-ui/core';
+import OptionsButton from '../OptionsButton';
+
+const InputWig = props => {
+  const { current, goal, targetDate, setIsEditingWig } = props;
+  const [selectedDate, setSelectedDate] = useState(targetDate);
+  const [currentNps, setCurrentNps] = useState(current);
+  const [goalNps, setGoalNps] = useState(goal);
+
+  const handleCancelClick = e => {
+    e.preventDefault();
+    setIsEditingWig(false);
+  };
+
+  const handleSaveClick = e => {
+    e.preventDefault();
+    setIsEditingWig(false);
+  };
+
+  const handleCurrentNpsChange = input => {
+    setCurrentNps(parseInt(input.target.value, 10));
+  };
+  const handleGoalNpsChange = input => {
+    setGoalNps(parseInt(input.target.value, 10));
+  };
+
+  const handleDateChange = date => {
+    setSelectedDate(moment(date).format('YYYY-MM-DD'));
+  };
+
+  const formStyle = {
+    border: '2px dotted white',
+    borderRadius: '10px',
+    padding: '10px',
+    width: '100%'
+  };
+
+  return (
+    <form style={formStyle}>
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <TextField
+          id="standard-number"
+          label="Current"
+          type="number"
+          style={{ color: '#ffff' }}
+          placeholder={currentNps.toString(10)}
+          default={currentNps.toString(10)}
+          fullWidth
+          variant="filled"
+          margin="normal"
+          onChange={handleCurrentNpsChange}
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+        <TextField
+          id="standard-number"
+          label="Goal"
+          type="number"
+          style={{ color: '#ffff' }}
+          placeholder={goalNps.toString(10)}
+          default={goalNps.toString(10)}
+          fullWidth
+          variant="filled"
+          margin="normal"
+          onChange={handleGoalNpsChange}
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+
+        <KeyboardDatePicker
+          label="Target Date"
+          format="YYYY-MM-DD"
+          fullWidth
+          margin="normal"
+          id="date-picker-inline"
+          variant="filled"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date'
+          }}
+        />
+      </MuiPickersUtilsProvider>
+      <div style={{ display: 'flex' }}>
+        <OptionsButton text="Save" onClick={handleSaveClick} />
+        <OptionsButton text="Cancel" onClick={handleCancelClick} />
+      </div>
+    </form>
+  );
+};
+
+InputWig.defaultProps = {
+  current: '0',
+  goal: '0',
+  targetDate: null
+};
+
+InputWig.propTypes = {
+  current: PropTypes.number,
+  goal: PropTypes.number,
+  targetDate: PropTypes.string,
+  setIsEditingWig: PropTypes.func.isRequired
+};
+
+export default InputWig;
