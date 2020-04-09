0ccbec749436ff86f6805c7eb38f1eef3d2b9005
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-09 12:13:25 +0200

    :construction: Update resource reducer

--- packages/frontend/src/reducers/resources.js
+++ packages/frontend/src/reducers/resources.js
@@ -1,13 +1,39 @@
-import { GET_RESOURCES } from '../actions/types';
+import { combineReducers } from 'redux';
+import { REQUEST_RESOURCES, RECEIVE_RESOURCES, RESOURCES_ERROR } from '../actions/types';
 
-const resourcesReducer = (state = null, action) => {
+const resources = (state = null, action) => {
   switch (action.type) {
-    case GET_RESOURCES:
-      console.log('GET_RESOURCES ACTION SENT');
+    case RECEIVE_RESOURCES:
       return action.payload;
     default:
       return state;
   }
 };
+
+const error = (state = null, action) => {
+  switch (action.type) {
+    case RESOURCES_ERROR:
+      return action.error;
+    default:
+      return state;
+  }
+};
+
+const isFetching = (state = false, action) => {
+  switch (action.type) {
+    case REQUEST_RESOURCES:
+      return true;
+    case RECEIVE_RESOURCES:
+      return false;
+    default:
+      return state;
+  }
+};
+
+const resourcesReducer = combineReducers({
+  resources,
+  isFetching,
+  error
+});
 
 export default resourcesReducer;
