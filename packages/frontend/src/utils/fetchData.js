1be3a015566f5f571ebe910412719f9d7bf64ed4
Amin Mushehid <amin.mushehid@vaimo.com>
2020-04-14 16:36:00 +0200

    Make a function that can be used for all requests made to the backend
    So that we keep the fetching DRY

--- packages/frontend/src/utils/fetchData.js
+++ packages/frontend/src/utils/fetchData.js
@@ -1,39 +1,32 @@
+// Helper for making requests to the backend
+export const backendFetch = async (urlPath, httpMethod = 'GET') => {
+  const url = `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_SERVER_PORT}/api/${urlPath}`;
+  const options = {
+    method: httpMethod,
+    headers: new Headers({ 'Content-Type': 'application/vnd.api+json' }),
+    redirect: 'follow'
+  };
+  try {
+    const response = await fetch(url, options);
+    return response.json();
+  } catch (error) {
+    return console.error(error);
+  }
+};
+
 /* 
 Add a response type key to each returned client array entry
 to make it easier to target the data
 */
-const labelResultsWithKey = (data, url) => {
-  const key = url.split('api/')[1];
-  const reformattedResult = {};
-  reformattedResult[key] = data;
-  return reformattedResult;
-};
+const labelResultsWithKey = (data, url) => ({ [url]: data });
 
+// Get all resources
 export default async () => {
-  const baseUrl = `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_SERVER_PORT}`;
-  const urls = [
-    `${baseUrl}/api/nps`,
-    `${baseUrl}/api/clients`,
-    `${baseUrl}/api/measures`,
-    `${baseUrl}/api/measureGoals`
-  ];
-  const myHeaders = new Headers();
-  myHeaders.append('Content-Type', 'application/vnd.api+json');
-
-  const requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  };
-
   const responseArr = await Promise.all(
-    urls.map(url =>
-      fetch(url, requestOptions)
-        .then(response => response.json())
-        .then(result => {
-          return labelResultsWithKey(result, url);
-        })
-    )
+    ['nps', 'clients', 'measures', 'measureGoals'].map(async url => {
+      const response = await backendFetch(url);
+      return labelResultsWithKey(response, url);
+    })
   );
 
   // Reshape into object with shape of: {nps: {...}, clients: {...} etc}
