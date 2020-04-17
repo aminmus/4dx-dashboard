a09d60414a9bb9ecffa2271d59d04e9b313e96a9
Amin Mushehid <amin.mushehid@vaimo.com>
2020-04-17 18:18:25 +0200

    Make updating of resources fully generic to work with all resource types

--- packages/frontend/src/slices/resources.js
+++ packages/frontend/src/slices/resources.js
@@ -1,19 +1,7 @@
-import { Deserializer, Serializer } from 'jsonapi-serializer';
 import { serializeError } from 'serialize-error';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import fetchData, { backendFetch } from '../utils/fetchData';
-
-const { deserialize } = new Deserializer({
-  keyForAttribute: 'camelCase'
-});
-
-const jsonapiSerializer = new Serializer('Nps', {
-  attributes: ['id', 'currentNps', 'goalNps', 'date', 'targetDate', 'createdAt', 'updatedAt']
-});
-
-const jsonapiDeserializer = new Deserializer({
-  keyForAttribute: 'camelCase'
-});
+import { serializePerType, deserialize } from '../utils/jsonapiSerializing';
 
 export const fetchResources = createAsyncThunk('resources/fetchResources', async () => {
   const { nps, clients, measures, measureGoals } = await fetchData();
@@ -27,20 +15,9 @@ export const fetchResources = createAsyncThunk('resources/fetchResources', async
 });
 
 export const updateResource = createAsyncThunk('resources/update', async ({ id, type, data }) => {
-  const requestBody = () => {
-    if (type === 'nps') {
-      return {
-        type: 'Nps',
-        currentNps: data.currentNps,
-        goalNps: data.goalNps,
-        targetDate: data.targetDate
-      };
-    }
-    return {};
-  };
-  const serializedRequest = await jsonapiSerializer.serialize(requestBody());
+  const serializedRequest = await serializePerType(type, data);
   const response = await backendFetch(`${type}/${id}`, 'PUT', serializedRequest);
-  const deserializedResponse = await jsonapiDeserializer.deserialize(response);
+  const deserializedResponse = await deserialize(response);
   const payload = { data: deserializedResponse, type };
   return payload;
 });
@@ -63,17 +40,16 @@ const resourcesSlice = createSlice({
       return { ...state, isFetching: true };
     },
     [updateResource.fulfilled]: (state, { payload }) => {
-      if (payload.type === 'nps') {
-        return {
-          ...state,
-          data: {
-            ...state.data,
-            nps: state.data.nps.map(entry => (entry.id === payload.data.id ? payload.data : entry))
-          },
-          isFetching: false
-        };
-      }
-      return { ...state, isFetching: false };
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          [payload.type]: state.data[payload.type].map(entry =>
+            entry.id === payload.data.id ? payload.data : entry
+          )
+        },
+        isFetching: false
+      };
     },
     [updateResource.rejected]: (state, { payload }) => {
       return { ...state, error: serializeError(payload), isFetching: false };
