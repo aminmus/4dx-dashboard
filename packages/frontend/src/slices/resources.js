908f0f8f7b406c4cc840e1bf7b68f5dc4a634b1d
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-20 15:41:19 +0200

    :construction: Add deleteResource function to resources slice

--- packages/frontend/src/slices/resources.js
+++ packages/frontend/src/slices/resources.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import fetchData, { backendFetch } from '../utils/fetchData';
 import { serializePerType, deserialize } from '../utils/jsonapiSerializing';
@@ -16,6 +17,13 @@ export const fetchResources = createAsyncThunk('resources/fetchResources', async
 export const updateResource = createAsyncThunk('resources/update', async ({ id, type, data }) => {
   const serializedRequest = await serializePerType(type, data);
   const response = await backendFetch(`${type}/${id}`, 'PUT', serializedRequest);
+  const deserializedResponse = await deserialize(response);
+  const payload = { data: deserializedResponse, type };
+  return payload;
+});
+
+export const deleteResource = createAsyncThunk('resources/delete', async ({ id, type }) => {
+  const response = await backendFetch(`${type}/${id}`, 'DELETE');
   const deserializedResponse = await deserialize(response);
   const payload = { data: deserializedResponse, type };
   return payload;
@@ -52,6 +60,25 @@ const resourcesSlice = createSlice({
     },
     [updateResource.rejected]: (state, { payload }) => {
       return { ...state, error: payload, isFetching: false };
+    },
+    [deleteResource.pending]: state => {
+      state.isFetching = true;
+      return state;
+    },
+    [deleteResource.fulfilled]: (state, { payload }) => {
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          [payload.type]: state.data[payload.type].filter(entry => entry.id !== payload.data.id)
+        },
+        isFetching: false
+      };
+    },
+    [deleteResource.rejected]: (state, { payload }) => {
+      state.isFetching = false;
+      state.error = payload;
+      return state;
     }
   }
 });
