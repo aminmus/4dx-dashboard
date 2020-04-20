6bb7a03e3bd9b99562ec37834fe93e26c6e7e178
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-20 14:57:54 +0200

    :construction: Add addResource action to resources

--- packages/frontend/src/slices/resources.js
+++ packages/frontend/src/slices/resources.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import fetchData, { backendFetch } from '../utils/fetchData';
 import { serializePerType, deserialize } from '../utils/jsonapiSerializing';
@@ -16,6 +17,14 @@ export const fetchResources = createAsyncThunk('resources/fetchResources', async
 export const updateResource = createAsyncThunk('resources/update', async ({ id, type, data }) => {
   const serializedRequest = await serializePerType(type, data);
   const response = await backendFetch(`${type}/${id}`, 'PUT', serializedRequest);
+  const deserializedResponse = await deserialize(response);
+  const payload = { data: deserializedResponse, type };
+  return payload;
+});
+
+export const addResource = createAsyncThunk('resources/create', async ({ type, data }) => {
+  const serializedRequest = await serializePerType(type, data);
+  const response = await backendFetch(`${type}`, 'POST', serializedRequest);
   const deserializedResponse = await deserialize(response);
   const payload = { data: deserializedResponse, type };
   return payload;
@@ -52,6 +61,20 @@ const resourcesSlice = createSlice({
     },
     [updateResource.rejected]: (state, { payload }) => {
       return { ...state, error: payload, isFetching: false };
+    },
+    [addResource.pending]: state => {
+      state.isFetching = false;
+      return state;
+    },
+    [addResource.fulfilled]: (state, { payload }) => {
+      state.data.nps.push(payload.data);
+      state.isFetching = false;
+      return state;
+    },
+    [addResource.rejected]: (state, { payload }) => {
+      state.error = payload;
+      state.isFetching = false;
+      return state;
     }
   }
 });
