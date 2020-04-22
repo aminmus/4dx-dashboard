ed7e1b5c4e7d6ce3e27b203327971976aa0ac92e
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-22 15:14:51 +0200

    :pencil: BE - Document Client Routes

--- packages/backend/src/routes/clients.js
+++ packages/backend/src/routes/clients.js
@@ -1,3 +1,11 @@
+/** Client routes
+ * @module Router/clients
+ * @requires express
+ * @requires ../controllers/client
+ * @requires ../middleware/authentication
+ * @requires ../middleware/permissions
+ */
+
 const router = require('express').Router();
 const {
   createOne,
@@ -9,12 +17,47 @@ const {
 const { checkAuth } = require('../middleware/authentication');
 const { canEdit } = require('../middleware/permissions');
 
+/**
+ * Get All Clients
+ *
+ * @name GetAllClients
+ * @route {GET} /api/clients
+ */
 router.get('/', getAll);
+
+/**
+ * GET One Client
+ *
+ * @name GetClient
+ * @route {GET} /api/clients/:id
+ */
 router.get('/:clientId', getById);
 
-// Protected routes
+/**
+ * UPDATE Client
+ *
+ * @name UpdateClient
+ * @route {PUT} /api/clients/:id
+ * @authentication This route requires JWT Authentication.
+ */
 router.put('/:clientId', checkAuth, canEdit, updateById);
+
+/**
+ * POST Client
+ *
+ * @name PostClient
+ * @route {POST} /api/clients
+ * @authentication This route requires JWT Authentication.
+ */
 router.post('/', checkAuth, canEdit, createOne);
+
+/**
+ * DELETE One Client
+ *
+ * @name DeleteClient
+ * @route {DELETE} /api/clients/:id
+ * @authentication This route requires JWT Authentication.
+ */
 router.delete('/:clientId', checkAuth, canEdit, deleteById);
 
 module.exports = router;
