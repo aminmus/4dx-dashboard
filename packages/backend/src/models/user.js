9630d2bcf98f2359438ea26ed4e2782afd4a7483
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-22 14:28:33 +0200

    :pencil: BE - Document User Model

--- packages/backend/src/models/user.js
+++ packages/backend/src/models/user.js
@@ -1,11 +1,30 @@
 /* eslint-disable no-param-reassign */
+
+/**
+ * User Model
+ * @module model_user
+ * @requires sequelize
+ * @requires bcryptjs
+ */
+
 const Sequelize = require('sequelize');
 const bcrypt = require('bcryptjs');
 
+/**
+ * Hash user password with brypt
+ * @function
+ * @memberof module:model_user
+ * @param {Object} user
+ */
 const hashPassword = async (user) => {
   user.password = await bcrypt.hash(user.password, 10);
 };
 
+/**
+ * Return Sequelized User Model with hashed password
+ * @param {Object} sequelize - sequelize module
+ * @param {Object} DataTypes - Type definitions for model properties
+ */
 module.exports = (sequelize, DataTypes) => {
   class User extends Sequelize.Model {
     isValidPassword(password) {
