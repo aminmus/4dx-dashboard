0184f1e455c8e487368ce3b909df0ebab8df3ffe
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-22 16:35:55 +0200

    :pencil: BE - Document User Routes

--- packages/backend/src/routes/users.js
+++ packages/backend/src/routes/users.js
@@ -1,16 +1,73 @@
+/** User routes
+ * @module Router/users
+ * @requires express
+ * @requires ../controllers/user
+ * @requires ../middleware/authentication
+ * @requires ../middleware/permissions
+ */
+
 const router = require('express').Router();
 const {
-  createOne, deleteById, getAll, getById, updateById,
+  createOne,
+  deleteById,
+  getAll,
+  getById,
+  updateById,
 } = require('../controllers/user');
 const { checkAuth } = require('../middleware/authentication');
 
 // Require authentication
 router.all('*', checkAuth);
 
+/**
+ * GET All Users
+ *
+ * @name GetAllUsers
+ * @route {GET} /api/users
+ * @authentication This route requires JWT Authentication.
+ */
 router.get('/', getAll);
+
+/**
+ * GET One User
+ *
+ * @name GetUser
+ * @route {GET} /api/users/:id
+ * @routeparam {String} :id The unique identifier for users
+ */
 router.get('/:userId', getById);
+
+/**
+ * UPDATE User
+ *
+ * @name UpdateUser
+ * @route {PUT} /api/users/:id
+ * @routeparam {String} :id The unique identifier for users
+ * @bodyparam {Object} data Sequelized User Data containing
+ *  type and attributes properties for resource
+ * @authentication This route requires JWT Authentication.
+ */
 router.put('/:userId', updateById);
+
+/**
+ * POST User
+ *
+ * @name PostUser
+ * @route {POST} /api/users
+ * @authentication This route requires JWT Authentication.
+ * @bodyparam {Object} data Sequelized User Data containing
+ *  type and attribute properties for resource
+ */
 router.post('/', createOne);
+
+/**
+ * DELETE One User
+ *
+ * @name DeleteUser
+ * @route {DELETE} /api/users/:id
+ * @routeparam {String} :id The unique identifier for users
+ * @authentication This route requires JWT Authentication.
+ */
 router.delete('/:userId', deleteById);
 
 module.exports = router;
