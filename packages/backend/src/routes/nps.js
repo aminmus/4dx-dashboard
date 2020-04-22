6b1608edfcd6c17ee6c278ec36977256caef1c82
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-22 16:35:20 +0200

    :pencil: BE - Document Nps Routes

--- packages/backend/src/routes/nps.js
+++ packages/backend/src/routes/nps.js
@@ -1,3 +1,11 @@
+/** Nps routes
+ * @module Router/nps
+ * @requires express
+ * @requires ../controllers/nps
+ * @requires ../middleware/authentication
+ * @requires ../middleware/permissions
+ */
+
 const router = require('express').Router();
 const {
   createOne,
@@ -9,12 +17,54 @@ const {
 const { checkAuth } = require('../middleware/authentication');
 const { canEdit } = require('../middleware/permissions');
 
+/**
+ * GET All Nps entries
+ *
+ * @name GetAllNps
+ * @route {GET} /api/nps
+ */
 router.get('/', getAll);
+
+/**
+ * GET One Nps entry
+ *
+ * @name GetNps
+ * @route {GET} /api/nps/:id
+ * @routeparam {String} :id The unique identifier for nps
+ */
 router.get('/:npsId', getById);
 
-// Protected routes
+/**
+ * UPDATE Nps entry
+ *
+ * @name UpdateMeasure
+ * @route {PUT} /api/nps/:id
+ * @routeparam {String} :id The unique identifier for nps
+ * @bodyparam {Object} data Sequelized Nps Data containing
+ *  type and attributes properties for resource
+ * @authentication This route requires JWT Authentication.
+ */
 router.put('/:npsId', checkAuth, canEdit, updateById);
+
+/**
+ * POST Nps entry
+ *
+ * @name PostNps
+ * @route {POST} /api/nps
+ * @authentication This route requires JWT Authentication.
+ * @bodyparam {Object} data Sequelized Nps Data containing
+ *  type and attribute properties for resource
+ */
 router.post('/', checkAuth, canEdit, createOne);
+
+/**
+ * DELETE One Nps entry
+ *
+ * @name DeleteNps
+ * @route {DELETE} /api/nps/:id
+ * @routeparam {String} :id The unique identifier for nps
+ * @authentication This route requires JWT Authentication.
+ */
 router.delete('/:npsId', checkAuth, canEdit, deleteById);
 
 module.exports = router;
