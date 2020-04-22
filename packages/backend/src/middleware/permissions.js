8902550d5ce847e2fc2dc4acf11658633ab8a871
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-22 14:04:28 +0200

    :pencil: BE - Document Permissions middleware

--- packages/backend/src/middleware/permissions.js
+++ packages/backend/src/middleware/permissions.js
@@ -1,5 +1,23 @@
+/**
+ * Permissions middlware
+ * @module middleware_permissions
+ * @requires ../utils/roles
+ */
+
+/**
+ * User role types
+ * @type {Object}
+ */
 const ROLES = require('../utils/roles');
 
+/**
+ * Check if user role permits them to edit data
+ * @function
+ * @memberof module:middleware_permissions
+ * @param {Object} req Request Object
+ * @param {Object} res - Express Request Object
+ * @param {Function} next - Express middleware.
+ */
 const canEdit = async (req, res, next) => {
   const { role } = await req.user;
 
