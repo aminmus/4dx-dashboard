18907b2f6f1bb6b10601a4190c140e85860143bf
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-23 10:37:33 +0200

    :wrench: BE - Document postgres Migrations

--- packages/backend/db/migrations/20200319155815-create-measure-goal.js
+++ packages/backend/db/migrations/20200319155815-create-measure-goal.js
@@ -1,26 +1,42 @@
+/**
+ * Migration for creating Measure Success Table
+ * @module Migrations_measure_success_table
+ */
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('MeasureGoals', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER,
-    },
-    measuresAmount: {
-      type: Sequelize.INTEGER,
-    },
-    targetDate: {
-      type: Sequelize.DATEONLY,
-      allowNull: false,
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-    },
-  }),
-  down: (queryInterface, _Sequelize) => queryInterface.dropTable('MeasureGoals'),
+    /**
+     * Create Measure Success Table
+     * @param {Object} queryInterface - Query interface for handling database operations
+     * @param {Object} Sequelize - Sequelize Object
+     */
+    up: (queryInterface, Sequelize) =>
+        queryInterface.createTable('MeasureGoals', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            measuresAmount: {
+                type: Sequelize.INTEGER
+            },
+            targetDate: {
+                type: Sequelize.DATEONLY,
+                allowNull: false
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        }),
+    /**
+     * Remove Measure Success Table
+     * @param {Object} queryInterface - Query interface for handling database operations
+     * @param {Object} _Sequelize - Sequelize Object
+     */
+    down: (queryInterface, _Sequelize) =>
+        queryInterface.dropTable('MeasureGoals')
 };
--- packages/backend/db/migrations/20200331132707-user-role.js
+++ packages/backend/db/migrations/20200331132707-user-role.js
@@ -1,14 +1,26 @@
 'use strict';
-
+/**
+ * Migration for creating Users role property
+ * @module Migrations_user_role
+ */
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('Users', 'role', {
-      type: Sequelize.STRING,
-      allowNull: false
-    });
-  },
-
-  down: (queryInterface, _Sequelize) => {
-    return queryInterface.removeColumn('Users', 'role');
-  }
+    /**
+     * Update User table with role property
+     * @param {Object} queryInterface - Query interface for handling database operations
+     * @param {Object} Sequelize - Sequelize Object
+     */
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.addColumn('Users', 'role', {
+            type: Sequelize.STRING,
+            allowNull: false
+        });
+    },
+    /**
+     * Remove User role property
+     * @param {Object} queryInterface - Query interface for handling database operations
+     * @param {Object} _Sequelize - Sequelize Object
+     */
+    down: (queryInterface, _Sequelize) => {
+        return queryInterface.removeColumn('Users', 'role');
+    }
 };
