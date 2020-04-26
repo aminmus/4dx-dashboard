6cd490ab1cb7bd8c8ddeb51e6439c2d21e2fd44a
Amin Mushehid <amin.mushehid@vaimo.com>
2020-04-26 20:20:31 +0200

    Add a custom login page component

---
+++ packages/frontend/src/layouts/Login.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { push } from 'connected-react-router';
+import { useNotify, Notification } from 'react-admin';
+import { ThemeProvider, Button } from '@material-ui/core';
+// eslint-disable-next-line import/no-cycle
+import authProvider from '../utils/react-admin/authProvider';
+import theme from '../style/muiTheme';
+
+/**
+ * Custom Login Page Component
+ * @component
+ */
+const Login = ({ dispatch }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const notify = useNotify();
+  const submit = async e => {
+    e.preventDefault();
+    try {
+      await authProvider.login({ username: email, password });
+      dispatch(push('/admin'));
+    } catch (error) {
+      notify('Invalid email or password', 'warning');
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <form onSubmit={submit}>
+        <div>
+          <label htmlFor="email">
+            Email
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              autoComplete="username"
+            />
+          </label>
+        </div>
+        <label htmlFor="password">
+          Password
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            autoComplete="current-password"
+          />
+        </label>
+        <Button type="submit">Sign in</Button>
+      </form>
+      <Notification />
+    </ThemeProvider>
+  );
+};
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired
+};
+
+export default connect(null, null)(Login);
