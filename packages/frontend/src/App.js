16269225c0a06c1a26b6b08c813b517267af06a0
Amin Mushehid <amin.mushehid@vaimo.com>
2020-04-27 19:54:34 +0200

    Change to connected-react-router and use customRoutes prop in react admin for home route
    This is so that we have a unified history with react-admin

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,16 +1,14 @@
 /* eslint-disable import/no-cycle */
 import React, { useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ReactRouterPropTypes from 'react-router-prop-types';
 import Header from './components/Header';
-import Home from './layouts/Home';
 import Admin from './layouts/Admin';
-import Login from './layouts/Login';
 import isAuthenticated from './utils/authentication';
 import { setLogoutStatus, setLoginStatus } from './actions/auth';
+import customRoutes from './customRoutes';
 
 /**
  * Main App Component
@@ -31,17 +29,7 @@ const App = ({ history, dispatch }) => {
   return (
     <ConnectedRouter history={history}>
       <Header />
-      <Switch>
-        <Route path="/admin">
-          <Admin history={history} />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Admin history={history} customRoutes={customRoutes} />
     </ConnectedRouter>
   );
 };
