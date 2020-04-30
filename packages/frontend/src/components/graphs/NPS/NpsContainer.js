7b8aa6e340732de131a0112e5c03a8ab0e2afd01
Daniel Salin <daniel.salin@chasacademy.com>
2020-04-30 11:30:43 +0200

    :construction: Strip redundant logic from NpsContainer component
    It now lives in the formatNpsData module

--- packages/frontend/src/components/graphs/NPS/NpsContainer.js
+++ packages/frontend/src/components/graphs/NPS/NpsContainer.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState, useRef } from 'react';
-import Chart from 'chart.js';
+import React, { useState } from 'react';
+import { Line } from 'react-chartjs-2';
 import PropTypes from 'prop-types';
 import { useMediaQuery } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import OptionsToggleButton from '../../elements/OptionsToggleButton';
-import OptionsButton from '../../elements/OptionsButton';
 import COLORS from '../../../style/COLORS';
 
-const { primary, light, success } = COLORS;
+const { primary, light } = COLORS;
 
-const Nps = props => {
+const NpsContainer = ({ npsChartData: { graphData, graphOptions } }) => {
   const match = useMediaQuery('(min-width:600px)');
+  const [optionsShow, setOptionsShow] = useState(false);
 
   /**
    * Component Styles
@@ -37,108 +37,14 @@ const Nps = props => {
 
   const classes = useStyles();
 
-  const { chart } = props;
-  const { months, target, values } = chart;
-
-  const chartContainer = useRef(null);
-  const [optionsShow, setOptionsShow] = useState(false);
-  const [chartInstance, setChartInstance] = useState(null);
-  const [displayTarget, setDisplayTarget] = useState(true);
-
-  const [npsDatasetData, setNpsDatasetData] = useState([]);
-  const [targetDatasetData, setTargetDatasetData] = useState([]);
-  const [labelsData, setLabelsData] = useState([]);
-
-  const chartConfig = {
-    type: 'line',
-    data: {
-      labels: labelsData,
-      datasets: [
-        {
-          data: npsDatasetData,
-          borderColor: primary,
-          borderWidth: 3,
-          pointRadius: match ? 5 : 1,
-          pointBorderWidth: 3,
-          pointBackgroundColor: primary,
-          fill: false
-        },
-        {
-          data: targetDatasetData,
-          spanGaps: true,
-          borderWidth: 2,
-          borderColor: success,
-          fill: false,
-          showLine: displayTarget,
-          pointRadius: displayTarget ? 2 : 0
-        }
-      ]
-    },
-    options: {
-      responsive: true,
-      scales: {
-        yAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: 'NPS',
-              fontColor: primary
-            },
-            ticks: {
-              beginAtZero: false,
-              suggestedMax: 30,
-              suggestedMin: -30
-            }
-          }
-        ],
-        xAxes: [
-          {
-            ticks: {
-              callback(value, _index, _values) {
-                return value.split(' ')[0];
-              }
-            }
-          }
-        ]
-      },
-      legend: {
-        display: false
-      },
-      fill: false
-    }
-  };
-
+  /**
+   * Toggle Options handler
+   * @param {Object} e - Event Input
+   */
   const toggleOptions = e => {
     e.preventDefault();
     setOptionsShow(!optionsShow);
   };
-  const optionsToggleTarget = e => {
-    e.preventDefault();
-    setDisplayTarget(!displayTarget);
-  };
-
-  useEffect(() => {
-    let targetArray = [];
-    if (target) {
-      targetArray = values.map(() => null);
-      targetArray[0] = target;
-      targetArray[values.length - 1] = target;
-    }
-
-    setTargetDatasetData(targetArray);
-    setLabelsData(months);
-    setNpsDatasetData(values);
-
-    if (chartContainer && chartContainer.current) {
-      const newChartInstance = new Chart(chartContainer.current, chartConfig);
-      setChartInstance(newChartInstance);
-    }
-    if (chartInstance) {
-      chartInstance.data.datasets[0].data = npsDatasetData;
-      chartInstance.data.datasets[1].data = targetDatasetData;
-      chartInstance.data.labels = labelsData;
-    }
-  }, [chartContainer, months, values, target, displayTarget, npsDatasetData]);
 
   return (
     <div className={classes.mainContainer}>
@@ -146,31 +52,26 @@ const Nps = props => {
         <div className="chart-title">Nps (Monthly)</div>
         <OptionsToggleButton onClick={toggleOptions} />
       </div>
+
       <div>
         {optionsShow && (
           <div className={classes.optionsContainer}>
-            <OptionsButton
-              text={displayTarget ? 'Hide Target' : 'Show Target'}
-              onClick={optionsToggleTarget}
-            />
+            <div>No options present</div>
           </div>
         )}
-        <canvas ref={chartContainer} />
+        <Line data={graphData} options={graphOptions} />
       </div>
     </div>
   );
 };
 
-Nps.defaultProps = {
-  chart: {}
+NpsContainer.propTypes = {
+  npsChartData: PropTypes.objectOf(
+    PropTypes.shape({
+      graphData: PropTypes.arrayOf(PropTypes.any),
+      graphOptions: PropTypes.objectOf(PropTypes.any)
+    })
+  ).isRequired
 };
 
-Nps.propTypes = {
-  chart: PropTypes.shape({
-    months: PropTypes.array,
-    values: PropTypes.array,
-    target: PropTypes.number
-  })
-};
-
-export default Nps;
+export default NpsContainer;
