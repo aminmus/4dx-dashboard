c050e1dec317fe76271274b42be02fa29b641624
Daniel Salin <daniel.salin@chasacademy.com>
2020-05-04 13:57:20 +0200

    :construction: Implement input validation in inputMeasuresGoal component

--- packages/frontend/src/components/elements/editMode/InputMeasuresGoal.js
+++ packages/frontend/src/components/elements/editMode/InputMeasuresGoal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
@@ -6,10 +6,27 @@ import MomentUtils from '@date-io/moment';
 import { TextField } from '@material-ui/core';
 import OptionsButton from '../OptionsButton';
 import formatDate from '../../../utils/formatDate';
+import { inputMeasuresGoalValidation } from '../../../utils/inputValidation';
 
+/**
+ * Measures Goal input component
+ *
+ * @component
+ * @param {Object} props Component props
+ * @param {String} props.id Unique identifier for Measures Goal resource (if used for editing)
+ * @param {Number} props.measures Current Nps
+ * @param {String} props.date Goal Nps
+ * @param {Function} props.handleSaveMeasureGoal Set whether or not user is editing a resource
+ * @param {Boolean} props.setIsEditing Set whether or not user is editing a resource
+ */
 const InputMeasuresGoal = ({ handleSaveMeasureGoal, setIsEditing, measures, date, id }) => {
   const [targetMeasures, setTargetMeasures] = useState(measures);
   const [selectedDate, setSelectedDate] = useState(date);
+
+  const [targetMeasuresErrorText, setTargetMeasuresErrorText] = useState();
+  const [targetDateErrorText, setTargetDateErrorText] = useState();
+
+  const [validationError, setValidationError] = useState(false);
 
   /**
    * Component Styles
@@ -41,21 +58,41 @@ const InputMeasuresGoal = ({ handleSaveMeasureGoal, setIsEditing, measures, date
     });
   };
 
+  useEffect(() => {
+    const { errors } = inputMeasuresGoalValidation(targetMeasures, selectedDate);
+    setTargetMeasuresErrorText(errors.targetMeasures);
+    setTargetDateErrorText(errors.targetDate);
+  }, [targetMeasures, selectedDate]);
+
+  /**
+   * Prevents user input if only one of the goal/target inputs are set
+   */
+  useEffect(() => {
+    if (targetDateErrorText || targetMeasuresErrorText) {
+      setValidationError(true);
+    } else {
+      setValidationError(false);
+    }
+  }, [targetDateErrorText, targetMeasuresErrorText]);
+
   return (
     <form className={classes.form}>
       <MuiPickersUtilsProvider utils={MomentUtils}>
         <TextField
           label="Target Measures"
-          value={targetMeasures.toString(10)}
-          default={targetMeasures.toString(10)}
           variant="filled"
           margin="normal"
           type="number"
           fullWidth
+          value={targetMeasures}
+          default={targetMeasures}
           onChange={input => setTargetMeasures(parseInt(input.target.value, 10))}
+          error={!!targetMeasuresErrorText}
+          helperText={targetMeasuresErrorText}
           InputLabelProps={{
             shrink: true
           }}
+          required
         />
         <KeyboardDatePicker
           label="Target Date"
@@ -66,14 +103,17 @@ const InputMeasuresGoal = ({ handleSaveMeasureGoal, setIsEditing, measures, date
           variant="filled"
           value={selectedDate}
           onChange={inputDate => setSelectedDate(formatDate(inputDate))}
+          error={!!targetDateErrorText}
+          helperText={targetDateErrorText}
           KeyboardButtonProps={{
             'aria-label': 'change date'
           }}
+          required
         />
       </MuiPickersUtilsProvider>
 
       <div className={classes.confirmContainer}>
-        <OptionsButton text="Save" onClick={handleSaveClick} />
+        <OptionsButton disabled={validationError} text="Save" onClick={handleSaveClick} />
         <OptionsButton text="Cancel" onClick={() => setIsEditing(false)} />
       </div>
     </form>
