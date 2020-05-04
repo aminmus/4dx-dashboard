9d381062efaa34a75cdc082fe98b4b580dc076ef
Daniel Salin <daniel.salin@chasacademy.com>
2020-05-04 14:44:23 +0200

    :lipstick: :pencil: Update EditButton hover style and add documentation

--- packages/frontend/src/components/elements/EditButton.js
+++ packages/frontend/src/components/elements/EditButton.js
@@ -3,10 +3,28 @@ import { makeStyles } from '@material-ui/core/styles';
 import EditIcon from '@material-ui/icons/Edit';
 import { IconButton } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import COLORS from '../../style/COLORS';
 
+const { danger } = COLORS;
+
+/**
+ * Edit Button component
+ *
+ * @component
+ * @param {Object} props Component props
+ * @param {Function} props.onClick On click event handler
+ */
 const EditButton = ({ onClick }) => {
   const useStyles = makeStyles({
-    iconBtn: { marginLeft: '10px', padding: '5px' }
+    iconBtn: {
+      marginLeft: '10px',
+      padding: '5px',
+      opacity: 0.5,
+      '&:hover': {
+        opacity: 1,
+        backgroundColor: danger
+      }
+    }
   });
 
   const classes = useStyles();
