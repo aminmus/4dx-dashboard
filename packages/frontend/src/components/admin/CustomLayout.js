5e7f99cca5765e3ccd4cb80dc67bc4cd4d87d5b9
Amin Mushehid <amin.mushehid@vaimo.com>
2020-05-05 15:43:15 +0200

    Set a more custom layout instead of depending on react-admins default layout component

--- packages/frontend/src/components/admin/CustomLayout.js
+++ packages/frontend/src/components/admin/CustomLayout.js
@@ -1,13 +1,80 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
-import { Layout } from 'react-admin';
-import CustomAppBar from './CustomAppBar';
-import Sidebar from './Sidebar';
+// /* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+// import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  // AppBar,
+  Menu,
+  Notification,
+  Sidebar,
+  setSidebarVisibility,
+  ComponentPropType
+} from 'react-admin';
 
-/**
- * Custom Layout component to pass into React Admin
- * @param {object} props
- */
-const CustomLayout = props => <Layout {...props} appBar={CustomAppBar} sidebar={Sidebar} />;
+const CustomLayout = ({ children, dashboard, logout }) => {
+  const useStyles = makeStyles(theme => ({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      zIndex: 1,
+      minHeight: '100vh',
+      backgroundColor: theme.palette.background.default,
+      position: 'relative'
+    },
+    appFrame: {
+      display: 'flex',
+      flexDirection: 'column',
+      overflowX: 'auto'
+    },
+    contentWithSidebar: {
+      display: 'flex',
+      flexGrow: 1
+    },
+    content: {
+      display: 'flex',
+      flexDirection: 'column',
+      flexGrow: 2,
+      padding: theme.spacing(3),
+      marginTop: '4em',
+      paddingLeft: 5
+    }
+  }));
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  // const open = useSelector(state => state.admin.ui.sidebarOpen);
+
+  useEffect(() => {
+    dispatch(setSidebarVisibility(true));
+  }, [setSidebarVisibility]);
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.appFrame}>
+        {/* <AppBar title={title} open={open} logout={logout} /> */}
+        <main className={classes.contentWithSidebar}>
+          <Sidebar>
+            <Menu logout={logout} hasDashboard={!!dashboard} />
+          </Sidebar>
+          <div className={classes.content}>{children}</div>
+        </main>
+        <Notification />
+      </div>
+    </div>
+  );
+};
+
+CustomLayout.defaultProps = {
+  children: null,
+  dashboard: null,
+  logout: null
+};
+
+CustomLayout.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]),
+  dashboard: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  logout: ComponentPropType
+};
 
 export default CustomLayout;
