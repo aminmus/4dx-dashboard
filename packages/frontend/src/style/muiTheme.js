d3e9a12503e875bb5b016aa3e095cd197466b3f4
Daniel Salin <daniel.salin@chasacademy.com>
2020-05-05 16:12:35 +0200

    :lipstick: Update muiTheme with typography styles

--- packages/frontend/src/style/muiTheme.js
+++ packages/frontend/src/style/muiTheme.js
@@ -1,14 +1,51 @@
+// @ts-nocheck
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
 import COLORS from './COLORS';
 
-const { primary, secondary } = COLORS;
+const { primary, secondary, light, gray } = COLORS;
 
 const theme = createMuiTheme({
+  textAlign: 'center',
   palette: {
     type: 'dark',
     primary: { main: primary },
     secondary: { main: secondary }
+  },
+  typography: {
+    h1: {
+      textAlign: 'center',
+      color: light,
+      marginBottom: '10px'
+    },
+    h2: {
+      textAlign: 'center',
+      color: light,
+      marginBottom: '10px'
+    },
+    h3: {
+      textAlign: 'center',
+      color: gray,
+      marginBottom: '10px'
+    },
+    h4: {
+      textAlign: 'center',
+      color: light
+    },
+    h5: {
+      textAlign: 'center',
+      color: light
+    },
+    h6: {
+      textAlign: 'center',
+      color: light
+    },
+    body1: {
+      color: light
+    },
+    body2: {
+      color: light
+    }
   }
 });
 
