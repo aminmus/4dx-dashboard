4359fb754ae1a4a60394af61861be4f9e6ea513b
Amin Mushehid <amin.mushehid@vaimo.com>
2020-05-12 18:12:02 +0200

    formatting

--- packages/backend/db/migrations/20200319155815-create-measure-goal.js
+++ packages/backend/db/migrations/20200319155815-create-measure-goal.js
@@ -3,40 +3,38 @@
  * @module Migrations_measure_success_table
  */
 module.exports = {
-    /**
+  /**
      * Create Measure Success Table
      * @param {Object} queryInterface - Query interface for handling database operations
      * @param {Object} Sequelize - Sequelize Object
      */
-    up: (queryInterface, Sequelize) =>
-        queryInterface.createTable('MeasureGoals', {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
-            measuresAmount: {
-                type: Sequelize.INTEGER
-            },
-            targetDate: {
-                type: Sequelize.DATEONLY,
-                allowNull: false
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            }
-        }),
-    /**
+  up: (queryInterface, Sequelize) => queryInterface.createTable('MeasureGoals', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    measuresAmount: {
+      type: Sequelize.INTEGER,
+    },
+    targetDate: {
+      type: Sequelize.DATEONLY,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  /**
      * Remove Measure Success Table
      * @param {Object} queryInterface - Query interface for handling database operations
      * @param {Object} _Sequelize - Sequelize Object
      */
-    down: (queryInterface, _Sequelize) =>
-        queryInterface.dropTable('MeasureGoals')
+  down: (queryInterface, _Sequelize) => queryInterface.dropTable('MeasureGoals'),
 };
