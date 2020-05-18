aa4d492290459e644a024d51a7dab49a35a8eea7
Amin Mushehid <amin.mushehid@vaimo.com>
2020-05-18 21:52:11 +0200

    Remove index.css and add baselinecss component from material ui

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 import { Provider } from 'react-redux';
 import { createBrowserHistory } from 'history';
 import App from './App';
